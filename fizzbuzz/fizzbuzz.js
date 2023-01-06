36cd7ba3125c0335d897f479dd558214a0142611
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-06 01:25:06 +0100

    refactor(fizzbuzz): simplify code, get rid of else statements

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,11 +1,6 @@
 export function fizzbuzz(number) {
-  if ((number % 3 === 0) && (number % 5 === 0)) {
-      return "fizzbuzz";
-  } else if (number % 3 === 0) {
-      return "fizz";
-  } else if (number % 5 === 0) {
-      return "buzz";
-  } else {
-      return number;
-  }
+  if ((number % 3 === 0) && (number % 5 === 0)) return "fizzbuzz";
+  if (number % 3 === 0) return "fizz";
+  if (number % 5 === 0) return "buzz";
+  return number;
 }
