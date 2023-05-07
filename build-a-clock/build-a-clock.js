063c1d96d678be210e37ac0ff045308916ae2390
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-05-07 10:38:42 +0200

    (build-a-clock): add initial state from Entwicklerheld

---
+++ build-a-clock/build-a-clock.js
@@ -0,0 +1,21 @@
+export class Clock {
+  constructor(hours=0, minutes=0) {
+    throw new Error('Implement me');
+  }
+
+  toString() {
+    throw new Error('Implement me');
+  }
+
+  plus(minutes) {
+    throw new Error('Implement me');
+  }
+
+  minus(minutes) {
+    throw new Error('Implement me');
+  }
+
+  equals(otherClock) {
+    throw new Error('Implement me');
+  }
+}
