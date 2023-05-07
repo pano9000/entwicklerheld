cf47a863a863640fa69eac2aec024d81e2af4a8a
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-05-07 12:46:08 +0200

    (build-a-clock): add initial solution

--- build-a-clock/build-a-clock.js
+++ build-a-clock/build-a-clock.js
@@ -1,21 +1,57 @@
 export class Clock {
+
+  #date;
   constructor(hours=0, minutes=0) {
-    throw new Error('Implement me');
+    this.#date = new Date();
+    this.#date.setHours(hours, minutes, 0);
+  }
+
+  #calcMinutes(mode, minutes) {
+
+    if (!Number.isInteger(minutes)) {
+      throw new Error(`Expected an Integer but received '${typeof(minutes)}'`)
+    }
+
+    if (["plus", "minus"].includes(mode) === false) {
+      throw new Error(`Invalid mode called.`)
+    }
+
+    const currentMinute = this.#date.getMinutes();
+    const newMinute = (mode === "plus") ? currentMinute + minutes : currentMinute - minutes;
+
+    this.#date.setMinutes(newMinute);
+    return this.toString();
+
   }
 
   toString() {
-    throw new Error('Implement me');
+    // @fix for some reason new Intl.DateTimeFormat does not work as expected here with node v11.15 - 
+    // it works at offline nodejs installation though, so reverting back to less fancy toTimeString slicing
+    //const timeFormat = new Intl.DateTimeFormat("de-DE", {timeStyle: "short"});
+    //console.log(this.#date, timeFormat.format(Date.now()))
+    //return timeFormat.format(this.#date);
+    return this.#date.toTimeString().slice(0,5)
   }
 
   plus(minutes) {
-    throw new Error('Implement me');
+    return this.#calcMinutes("plus", minutes);
   }
 
   minus(minutes) {
-    throw new Error('Implement me');
+    return this.#calcMinutes("minus", minutes);
   }
 
   equals(otherClock) {
-    throw new Error('Implement me');
+    if (otherClock instanceof Clock === false) {
+      throw new Error(`Invalid comparison clock provided: The comparison clock has to be an instance of 'Clock'`)
+    }
+    
+    const pairs = [
+      [this.#date.getMinutes(), otherClock.#date.getMinutes()],
+      [this.#date.getHours(), otherClock.#date.getHours()]
+    ]
+
+    return pairs.every(pair => pair[0] === pair[1])
+    
   }
 }
