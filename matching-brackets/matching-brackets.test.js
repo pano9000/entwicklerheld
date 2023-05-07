048cc5499c82e2861dd6a6c5dc1fdc38fdd2d722
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-05-07 15:59:33 +0200

    (matching-brackets): adapt tests to mocha syntax

--- matching-brackets/matching-brackets.test.js
+++ matching-brackets/matching-brackets.test.js
@@ -1,31 +1,24 @@
-import {isPaired} from '../matching-brackets';
+import {isPaired} from './matching-brackets.js';
 import { expect } from 'chai';
 
-beforeEach(() => {
-    console.log("##polylith[testStarted");
-});
-
-afterEach(() => {
-    console.log("##polylith[testFinished")
-});
 // Scenario 1
-test('empty string', () => {
+it('empty string', () => {
     expect(isPaired('', "The empty string '' should result in true")).to.equal(true);
 });
 
 // Scenario 2
-test('paired square brackets', () => {
+it('paired square brackets', () => {
     expect(isPaired('[]', "The input '[]' should result in true")).to.equal(true);
 });
-test('paired curly braces', () => {
+it('paired curly braces', () => {
     expect(isPaired('{}', "The input '{}' should result in true")).to.equal(true);
 });
-test('paired brackets', () => {
+it('paired brackets', () => {
     expect(isPaired('()', "The input '()' should result in true")).to.equal(true);
 });
 
 // Scenario 2 - other tests
-test('paired square brackets1', () => {
+it('paired square brackets1', () => {
     expect(isPaired('[({})]'), "The input '[({})]' should result in true").to.equal(true);
     expect(isPaired('{ }'), "The input '{ }' should result in true").to.equal(true);
     expect(isPaired('{[]}'), "The input '{[]}' should result in true").to.equal(true);
@@ -34,20 +27,20 @@ test('paired square brackets1', () => {
 });
 
 // Scenario 3
-test('unpaired brackets', () => {
+it('unpaired brackets', () => {
     expect(isPaired('[['), "The input '[[' should result in false").to.equal(false);
 });
 
-test('wrong ordered brackets', () => {
+it('wrong ordered brackets', () => {
     expect(isPaired('}{'), "The input '}{' should result in false").to.equal(false);
 });
 
-test('wrong closing bracket', () => {
+it('wrong closing bracket', () => {
     expect(isPaired('{)'), "The input '{)' should result in false").to.equal(false);
 });
 
 // Scenario 3 - other tests
-test('partially paired brackets', () => {
+it('partially paired brackets', () => {
     expect(isPaired('{[])'), "The input '{[])' should result in false").to.equal(false);
     expect(isPaired('{[)][]}'), "The input '{[)][]}' should result in false").to.equal(false);
     expect(isPaired('({[)][]}'), "The input '({[)][]}' should result in false").to.equal(false);
@@ -56,7 +49,7 @@ test('partially paired brackets', () => {
 });
 
 // Scenario 4
-test('complex latex expression', () => {
+it('complex latex expression', () => {
     expect(
         isPaired(
             '\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)'
@@ -64,12 +57,12 @@ test('complex latex expression', () => {
     ).to.equal(true);
 });
 
-test('math expression', () => {
+it('math expression', () => {
     expect(isPaired('(((185 + 223.85) * 15) - 543)/2'), "The input '(((185 + 223.85) * 15) - 543)/2' should result in true").to.equal(true);
 });
 
 // Scenario 4 - other tests
-test('paired and nested brackets', () => {
+it('paired and nested brackets', () => {
     expect(isPaired('([{}({}[])])'), "The input '([{}({}[])])' should result in true").to.equal(true);
     expect(isPaired('{[)][]}'), "The input '{[)][]}' should result in false").to.equal(false);
     expect(isPaired('([{])'), "The input '([{])' should result in false").to.equal(false);
