c8fbb2d756f8b9d4671a89da62a06ff5e2a346ea
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-06-28 09:35:02 +0200

    (say-numbers-js):  dump reworked version, needs cleanup, but works

--- say-numbers-js/say-numbers.js
+++ say-numbers-js/say-numbers.js
@@ -2,76 +2,56 @@
 Transform integer numbers to their english word!
 */
 
-function getWord(number) {
+export default function getWord(number) {
 
 
-  const numbStr = String(number)
-  const numbLen = numbStr.length
+  const numbStr = String(number);
+  const numbLen = numbStr.length;
   console.log("\n-----\nstartnumber", numbStr, "\n----");
-  //console.log(numbLen, numbStr, numbLen % 3)
 
-const tens = new Map([
-  [0, "zero"],
-  [1, "one"],
-  [2, "two"],
-  [3, "three"],
-  [4, "four"],
-  [5, "five"],
-  [6, "six"],
-  [7, "seven"],
-  [8, "eight"],
-  [9, "nine"],
-  [1_0, "ten"],
-  [1_1, "eleven"],
-  [1_2, "twelve"],
-  [1_3, "thirteen"],
-  [1_4, "fourteen"],
-  [1_5, "fifteen"],
-  [1_6, "sixteen"],
-  [1_7, "seventeen"],
-  [1_8, "eighteen"],
-  [1_9, "nineteen"],
+  const dict = [];
 
-]);
+  dict[0] = new Map([
+    [0, "zero"],
+    [1, "one"],
+    [2, "two"],
+    [3, "three"],
+    [4, "four"],
+    [5, "five"],
+    [6, "six"],
+    [7, "seven"],
+    [8, "eight"],
+    [9, "nine"],
+    [1_0, "ten"],
+    [1_1, "eleven"],
+    [1_2, "twelve"],
+    [1_3, "thirteen"],
+    [1_4, "fourteen"],
+    [1_5, "fifteen"],
+    [1_6, "sixteen"],
+    [1_7, "seventeen"],
+    [1_8, "eighteen"],
+    [1_9, "nineteen"],
+  ]);
 
-const tys = new Map([
-  [2, "twenty"],
-  [3, "thirty"],
-  [4, "fourty"],
-  [5, "fifty"],
-  [6, "sixty"],
-  [7, "fifty"],
-  [8, "eighty"],
-  [9, "ninety"],
-]);
+  dict[1] = new Map([
+    [2, "twenty"],
+    [3, "thirty"],
+    [4, "fourty"],
+    [5, "fifty"],
+    [6, "sixty"],
+    [7, "fifty"],
+    [8, "eighty"],
+    [9, "ninety"],
+  ]);
 
-const biggies = new Map([
-  [0, "hundred"],
-  [1, "thousand"],
-  [2, "million"],
-  [3, "billion"],
-  [4, "trillion"],
-])
+  dict[2] = new Map([
+    [0, "hundred"],
+    [1, "thousand"],
+    [2, "million"],
+    [3, "billion"],
+  ])
 
-const getStuff = (depth, pos) => {
-
-
-  
-}
-
-/*
-let depth = -1;
-for (let i = numbLen; i > 0; i--) {
-  depth++;
-  console.log(
-    depth,
-    numbStr.charAt(i-1).padEnd(depth+1, "0"),
-    tens.get(
-      parseInt(numbStr.charAt(i-1))
-    )
-  )
-}
-*/
 
 const splitIntoArray = () => {
   let j = 0;
@@ -84,11 +64,9 @@ const splitIntoArray = () => {
     } 
     j++;
     if (!arr[arrDepth]) {
-      //arr[arrDepth] = ["", "", ""]
       arr[arrDepth] = [];
     };
     arr[arrDepth].push(numbStr[i-1])
-    //console.log(i, j, arr, arrDepth)
 
     //pad the array with 0
     if (i === 1 && arr[arrDepth].length < 3) {
@@ -104,9 +82,14 @@ const splitIntoArray = () => {
 
 const getStringFromSplitArray = (splitArr) => {
 
-  console.log("shplitarr", splitArr)
+  console.log("shplitarr", 
+    splitArr, 
+    splitArr.length === 1,
+    splitArr[0].every(item => item == 0)
+  )
 
   const strArr = [];
+
   for (let block = splitArr.length - 1; block > -1; block--) {
 
     console.log("blocki", splitArr[block])
@@ -115,20 +98,36 @@ const getStringFromSplitArray = (splitArr) => {
     };
 
 
-//    for (let numIndex = 0; numIndex < splitArr[block].length; numIndex++) {
-    for (let numIndex = splitArr[block].length -1 ; numIndex > -1; numIndex--) {
+    //speical case for zero
+    if (splitArr.length === 1 && splitArr[0].every(item => item == 0)) {
+      strArr[block].push(dict[0].get(0))
+      return strArr
+    }
+
+    const currBlock = splitArr[block];
+    strArr[block].push(getWordOfBlock(splitArr[block]).join(" "))
+    console.log("block0word", getWordOfBlock(splitArr[block]))
+
+    if (block !== 0) {
+      console.log("yo", dict[2].get( parseInt( block )))
+      strArr[block].push(dict[2].get( parseInt( block )))
+    }
+    strArr[block] = strArr[block].join(" ")
+
+    //    for (let numIndex = 0; numIndex < splitArr[block].length; numIndex++) {
+/*     for (let numIndex = splitArr[block].length -1 ; numIndex > -1; numIndex--) {
       console.log("num", numIndex, splitArr[block][numIndex])
 
       const currNumber = splitArr[block][numIndex];
       const prevNumber = splitArr[block][numIndex-1];
 
-      if (currNumber == 0) {
+      if (numIndex !== 2 && currNumber == 0) {
         continue;
       }
 
-      if (numIndex === 2) {
-        let suffix = biggies.get( parseInt( 0 ));
-        let currStr = tens.get( parseInt(currNumber  ));
+      if (numIndex === 2 &&  splitArr[block][2] != 0) {
+        let suffix = dict[2].get( parseInt( 0 ));
+        let currStr = dict[0].get( parseInt(currNumber  ));
         console.log("num in hun", currNumber, "currBlock", block, suffix);
         strArr[block].push(currStr, suffix);
 
@@ -138,72 +137,81 @@ const getStringFromSplitArray = (splitArr) => {
 
       if (numIndex === 1) {
         console.log("second pos", currNumber+prevNumber)
-        const specNum = tens.get( parseInt(currNumber+prevNumber))
+        const specNum = dict[0].get( parseInt(currNumber+prevNumber))
         if (specNum !== undefined) {
           console.log("sepcnum", specNum)
           strArr[block].push(specNum);
           break;
         }
-        const tysStr = tys.get(parseInt(currNumber));
-        console.log("tys", tysStr);
-        strArr[block].push(tysStr);
+        const tysStr = dict[1].get(parseInt(currNumber));
+        const abc = dict[0].get( parseInt(prevNumber));
+        const tensStr = (abc == "zero") ? "" : "-"+abc
+        console.log("tys", tysStr, "tens", abc);
+        strArr[block].push(`${tysStr}${tensStr}`);
+        break;
         continue;
       }
 
-      let str = tens.get( parseInt(currNumber  ));
-
+      let str = dict[0].get( parseInt(currNumber  ));
+      console.log("yo")
       strArr[block].push(str)
-    }
+    } */
 
-    if (block !== 0) {
-      console.log("yo", biggies.get( parseInt( block )))
-      strArr[block].push(biggies.get( parseInt( block )))
-    }
+
 
   }
+  console.log(strArr.reverse().join(" "))
+  return strArr.reverse().join(" ")
 
-  return strArr
-
-  //console.log("a", strArr)
 }
 
-const split =   splitIntoArray();
+const split = splitIntoArray();
 console.log("splitt", split)
 const stringi = getStringFromSplitArray(split);
 
 console.log("\n-----\nstartnumber", numbStr, "\nstringi", stringi);
 
-//console.log("a", splitIntoArray())
+function getWordOfBlock(block) {
+  const blockResult = []
+  if (block[2] != 0) {
+    console.log("block2", block[2])
+    blockResult.push(dict[0].get(parseInt(block[2])), dict[2].get(0))
+  }
 
-/*console.log(
+  console.log("getbloy", block[1]+block[0], parseInt(block[1]+block[0]))
+  if (block[1] == "1") {
 
+    if (dict[0].get(parseInt(block[1]+block[0])) !== undefined) {
+      blockResult.push(dict[0].get(parseInt(block[1]+block[0])));
+      return blockResult;
+    }
+  }
 
-);
-*/
+  if (block[1] !== "0") {
+    const rest = `${dict[1].get(parseInt(block[1]))}-${dict[0].get(parseInt(block[0]))}`
+    blockResult.push(rest)
+    return blockResult
+  }
 
-//arr.reverse()
+  if (block[0] !== "0") {
+    blockResult.push(dict[0].get(parseInt(block[0])))
+    return blockResult
+  }
 
-
-
-//[ [ '1', '1' ], [ '9', '4', '1' ] ]
-
-/*
-[ '9', '4', '1' ]
-
-0 -> take 1 series and append "hundred"
-1 -> check if num > 1
-      -> if yes -> take arr[1] + append 0, then search in 1_0 series
-      -> if no -> take arr[1] & arr[2] and 
-2 -> take 1 series -> no appending
-
-*/
-
-
-//console.log(tens.get(2), tens.get(1_000), tens.get(40), tens.get(9))
-//[1_100_000]
+  return blockResult
 
 }
 
+
+
+/**
+ * split
+ * getStringsFromArray
+ * printString
+ */
+
+}
+/*
 getWord(513)
 getWord(41)
 getWord(234)
@@ -212,13 +220,21 @@ getWord(1_519)
 getWord(102_519)
 getWord(123_519)
 getWord(13_519)
-getWord(5_123_519)
+
 getWord(51_123_519)
 getWord(523_123_519)
 getWord(10_516_123_519)
 getWord(1_100_516_123_519)
+getWord(987_654_321_123)
+getWord(222)
+*/
 
-
+getWord(0)
+getWord(1)
+getWord(12)
+getWord(10)
+getWord(100)
+getWord(5_123_519)
 
 
 //getWord(52123519)
