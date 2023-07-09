2a305a3201f5f27af306c93e8ca03a520d22add9
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-07-09 13:26:59 +0200

    (change-calculator): add initial state

---
+++ change-calculator/ChangeCalculator.java
@@ -0,0 +1,17 @@
+package de.entwicklerheld.changeCalculatorJava;
+
+
+import java.util.*;
+
+public class ChangeCalculator {
+    private final List<Integer> coins;
+
+    public ChangeCalculator(List<Integer> coins) {
+        this.coins = coins;
+    }
+
+    public List<Integer> computeMostEfficientChange(Integer amount) {
+       return null;
+    }
+
+}
