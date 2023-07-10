31b958d29177763779a19114e5503b39de9d2aff
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-07-11 01:37:34 +0200

    (state-machine): cleanup solution

--- state-machine/PolicyRequest.java
+++ state-machine/PolicyRequest.java
@@ -4,15 +4,17 @@ import java.util.Set;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.math.BigInteger;
-import java.util.ArrayList;
 
 public class PolicyRequest extends AbstractPolicyRequest implements IPolicyRequest {
     String currentState;
-    HashMap<String, Set<String>> stateTransitions = new HashMap<String, Set<String>>();
+    static HashMap<String, Set<String>> stateTransitions = new HashMap<String, Set<String>>();
+
+    static {
+        initStateTransitions();
+    }
 
     public PolicyRequest(String state) {
         this.currentState = state;
-        this.initStateTransitions();
     }
 
     @Override
@@ -36,9 +38,9 @@ public class PolicyRequest extends AbstractPolicyRequest implements IPolicyReque
         return isTransitionPossible && isIBANValid;
     }
 
-    private void initStateTransitions() {
+    static private void initStateTransitions() {
 
-        String[] states = {
+        String[] transitionStates = {
             "INCOMPLETE",
             "NEW",
             "IN_REVIEW",
@@ -52,36 +54,33 @@ public class PolicyRequest extends AbstractPolicyRequest implements IPolicyReque
             "DUPLICATE_FAKE"
         };
 
-
-        for (String state : states) {
-            this.stateTransitions.put(state, new HashSet<String>());
-            if (
-                (state != "NOT_SUCCESSFUL") 
+        for (String state : transitionStates) {
+            stateTransitions.put(state, new HashSet<String>());
+            if ((state != "NOT_SUCCESSFUL") 
                 && (state != "ERROR") 
                 && (state != "CANCELED")
                 && (state != "INACTIVE")
-                && (state != "DUPLICATE_FAKE")
-                ) {
-                this.stateTransitions.get(state).add("ERROR");
+                && (state != "DUPLICATE_FAKE")) {
+                    stateTransitions.get(state).add("ERROR");
             }
         }
         
-        this.stateTransitions.get("INCOMPLETE").add("NEW");
-        this.stateTransitions.get("INCOMPLETE").add("DUPLICATE_FAKE");
+        stateTransitions.get("INCOMPLETE").add("NEW");
+        stateTransitions.get("INCOMPLETE").add("DUPLICATE_FAKE");
 
-        this.stateTransitions.get("NEW").add("DUPLICATE_FAKE");
-        this.stateTransitions.get("NEW").add("IN_REVIEW");
+        stateTransitions.get("NEW").add("DUPLICATE_FAKE");
+        stateTransitions.get("NEW").add("IN_REVIEW");
 
-        this.stateTransitions.get("IN_REVIEW").add("READY_FOR_TRANSMISSION");
-        this.stateTransitions.get("IN_REVIEW").add("DUPLICATE_FAKE");
+        stateTransitions.get("IN_REVIEW").add("READY_FOR_TRANSMISSION");
+        stateTransitions.get("IN_REVIEW").add("DUPLICATE_FAKE");
 
-        this.stateTransitions.get("READY_FOR_TRANSMISSION").add("TRANSMITTED");
+        stateTransitions.get("READY_FOR_TRANSMISSION").add("TRANSMITTED");
 
-        this.stateTransitions.get("TRANSMITTED").add("SUCCESSFUL");
-        this.stateTransitions.get("TRANSMITTED").add("NOT_SUCCESSFUL");
+        stateTransitions.get("TRANSMITTED").add("SUCCESSFUL");
+        stateTransitions.get("TRANSMITTED").add("NOT_SUCCESSFUL");
 
-        this.stateTransitions.get("SUCCESSFUL").add("INACTIVE");
-        this.stateTransitions.get("SUCCESSFUL").add("CANCELED");
+        stateTransitions.get("SUCCESSFUL").add("INACTIVE");
+        stateTransitions.get("SUCCESSFUL").add("CANCELED");
 
     }
 
@@ -89,25 +88,20 @@ public class PolicyRequest extends AbstractPolicyRequest implements IPolicyReque
     private Boolean isValidIBAN(String iban) {
         if (iban == null) return false;
         String shiftedIBAN = iban.substring(4) + iban.substring(0, 4);
-        String ibanInts = replaceIBANLetterByNumber(shiftedIBAN);
+        String replacedIBAN = replaceIBANLetterByNumber(shiftedIBAN);
         BigInteger modulo97 = new BigInteger("97");
-        Boolean isValidIBAN = (new BigInteger(ibanInts).remainder(modulo97).intValue() == 1);
-        System.out.println("sys" + " " + iban + "//" + new BigInteger(ibanInts).remainder(modulo97).intValue() + "//" + isValidIBAN);
+        Boolean isValidIBAN = (new BigInteger(replacedIBAN).remainder(modulo97).intValue() == 1);
 
         return isValidIBAN;
     }
 
     private String replaceIBANLetterByNumber(String iban) {
-
-        ArrayList<Integer> ibanInts = new ArrayList<Integer>();
-        String ibans = "";
-        for (Character letter : iban.toCharArray()) {
-            ibanInts.add(Character.getNumericValue(letter));
-            ibans += Character.getNumericValue(letter);
+        String replacedIBAN = "";
+        for (Character letter : iban.toUpperCase().toCharArray()) {
+            replacedIBAN += Character.getNumericValue(letter);
         }
 
-        return ibans;
-
+        return replacedIBAN;
     }
 
 
