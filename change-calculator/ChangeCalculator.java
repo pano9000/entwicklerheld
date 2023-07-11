dc21c151be14190979b1ed8a0ee44914b75e8f75
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-07-11 08:43:42 +0200

    (change-calculator): add Java solution

--- change-calculator/ChangeCalculator.java
+++ change-calculator/ChangeCalculator.java
@@ -11,7 +11,41 @@ public class ChangeCalculator {
     }
 
     public List<Integer> computeMostEfficientChange(Integer amount) {
-       return null;
+        List<Integer> resultList = new ArrayList<>();
+        
+        if (amount == 0) return resultList;
+        
+        resultList = getBestPath(amount, this.coins, new HashMap<Integer, List<Integer>>());
+
+        if (resultList == null || resultList.size() == 0) {
+            throw new IllegalArgumentException("Coins cannot be split into the given amount");
+        }
+
+        return resultList;
+    }
+
+
+    private List<Integer> getBestPath(Integer target, List<Integer> coins, HashMap<Integer, List<Integer>> memo) {
+      if (memo.get(target) != null) return memo.get(target);
+      if (target == 0) return new ArrayList<>();
+      if (target < 0) return null;
+
+      List<Integer> shortestPath = null;
+
+      for (int coin : coins) {
+        int remainder = target - coin;
+        List<Integer> remainderPath = getBestPath(remainder, coins, memo);
+        if (remainderPath != null) {
+            List<Integer> currentPath = new ArrayList<>(remainderPath);
+            currentPath.add(coin);
+            if (shortestPath == null || currentPath.size() < shortestPath.size()) {
+                shortestPath = currentPath;
+            }
+        }
+      }
+
+      memo.put(target, shortestPath);
+      return shortestPath;
     }
 
 }
