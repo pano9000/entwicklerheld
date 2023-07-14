d4788f7139da8850b676ce6188efdf0ab360fe98
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-07-14 08:50:29 +0200

    (maps-and-polygons): add solution for first test case

--- maps-and-polygons/MapService.java
+++ maps-and-polygons/MapService.java
@@ -7,8 +7,19 @@ public class MapService implements Drawable {
 
 
     public static Double calculatePolygonArea(Point[] vertices) {
-        // Scenario 1
-        return null;
+        // calculating the area of an irregular polygon with the shoelace algorithm
+        // multiply the y coordinate of each row with the x coordinate in the row below
+        // multiply the x coordinate of each row with the y coordinate in the row below
+        // subtract second sum from first, absolute value, then divide result by 2 -> area
+        
+        double sum1 = 0, sum2 = 0;
+        for (int i = 0; i <= vertices.length-1; i++) {
+            int nextIndex = (i != vertices.length-1) ? i+1 : 0;
+            sum1 += vertices[i].y() * vertices[nextIndex].x();
+            sum2 += vertices[i].x() * vertices[nextIndex].y();
+        }
+
+        return Math.abs(sum2 - sum1) / 2;
     }
 
     public static void calculateAndSetLabelsForPolygons(List<String> labelNames) {
