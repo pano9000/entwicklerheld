03d786f493406201ab7282dcd86a750e443b425c
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-07-21 23:34:25 +0200

    (github-actions/getStatsFromEntwicklerHeld): also process single README.md files

--- .github/scripts/getStatsFromEntwicklerHeld.js
+++ .github/scripts/getStatsFromEntwicklerHeld.js
@@ -5,16 +5,15 @@ import { writeFileSync }  from "fs";
 async function getStatsFromEntwicklerHeld() {
 
   const processedData = await processData(challengeData);
-  const overviewMarkdown = createOverviewMarkdown(processedData);
-  //console.log(processedData, JSON.stringify(processedData), overviewMarkdown)
-  writeReadmeMd(overviewMarkdown);
+  createOverviewMarkdownFile(processedData);
+  createSingleMarkdownFiles(processedData);
 
 }
 
 
-function writeReadmeMd(content) {
+function writeReadmeMd(file, content) {
   try {
-    writeFileSync("./README.md", content);
+    writeFileSync(file, content);
   }
   catch(err) {
     console.err(err);
@@ -90,22 +89,24 @@ async function processData(challengeData) {
 }
 
 
-function createOverviewMarkdown(data) {
+function createOverviewMarkdownFile(data) {
 
   const lines = [];
 
   lines.push("# pano9000 EntwicklerHeld Challenges")
   lines.push("");
-  lines.push("I use this repository to save and present my solutions for the challenges on the [EntwicklerHeld](https://platform.entwicklerheld.de/) coding challenge platform.")
-  lines.push("")
-  lines.push("Click on the challenge names for more details and to see the solution")
+  lines.push("I use this repository to save and present my solutions for the challenges on the [EntwicklerHeld](https://platform.entwicklerheld.de/) coding challenge platform.");
+  lines.push("");
+  lines.push("Click on the challenge names for more details and to see the solution.")
+  lines.push(`List Last updated: ${new Date().toISOString().slice(0,10)}`);
+  lines.push("");
   lines.push("### Difficulty Levels (According to EntwicklerHeld)");
   lines.push("");
   lines.push("▮▯▯▯ = `Easy`", "", "▮▮▯▯ = `Medium`", "", "▮▮▮▯ = `Hard`", "", "▮▮▮▮ = `Hardcore`", "");
-  lines.push("## Solved Challenges Sorted by Solution Date")
-  lines.push("")
-  lines.push("Challenge Name | Difficulty | Overall Success Rate @EH | Overall Accepted / Solved @EH | Date Solved | Language")
-  lines.push("---|---|---|---|---|---|");
+  lines.push("## Solved Challenges Sorted by Solution Date");
+  lines.push("");
+  lines.push("Challenge Name | Difficulty | Overall Success Rate @EH | Overall Accepted / Solved @EH | Date Solved | Language");
+  lines.push(getTableHeadingMarkdown(lines));
 
   data.forEach(entry => {
     lines.push(`[${entry.title}](${entry.urlSolution}) | ${entry.difficulty.bar} | ${entry.statistics.succesRateBar} ${entry.statistics.succesRate}% | ${entry.statistics.acceptancesCount} / ${entry.statistics.solutionsCount} | ${entry.dateSolved} | ${entry.language}`)
@@ -114,7 +115,9 @@ function createOverviewMarkdown(data) {
   lines.push("## Solved Challenges Sorted by Difficulty, then Solution Date")
   lines.push("")
   lines.push("Challenge Name | Difficulty | Overall Success Rate @EH | Overall Accepted / Solved @EH | Date Solved | Language")
-  lines.push("---|---|---|---|---|---|");
+  lines.push(getTableHeadingMarkdown(lines));
+
+
 
 
   // sort descending by difficulty first, then by solution date
@@ -126,8 +129,46 @@ function createOverviewMarkdown(data) {
 
 
 
-  return lines.join("\n");
+  const content = lines.join("\n");
 
+  writeReadmeMd("./README.md", content);
+
+}
+
+
+function createSingleMarkdownFiles(data) {
+
+  data.forEach(entry => {
+    const lines = [];
+
+    lines.push(`# ${entry.title}`)
+    lines.push("");
+    lines.push(`My solution for the ['${entry.title}' Challenge](${entry.urlEH}) on the [EntwicklerHeld](https://platform.entwicklerheld.de/) Code Challenge Platform.`);
+    lines.push("");
+    lines.push("## Description");
+    lines.push(entry.description.replaceAll("\\n", "\n\n"));
+    lines.push("");
+    lines.push("## Challenge Info")
+    lines.push("Difficulty | Overall Success Rate @EH | Overall Accepted / Solved @EH | Date Solved | Language");
+    lines.push(getTableHeadingMarkdown(lines));
+    lines.push(`${entry.difficulty.bar} | ${entry.statistics.succesRateBar} ${entry.statistics.succesRate}% | ${entry.statistics.acceptancesCount} / ${entry.statistics.solutionsCount} | ${entry.dateSolved} | ${entry.language}`);
+    lines.push("");
+    lines.push("## Comment");
+    lines.push(entry.comment.replaceAll("\\n", "\n\n"));
+
+    const content = lines.join("\n");
+    const folder = entry.urlSolution.split("/").at(-1)
+    writeFileSync(`./${folder}/README.md`, content);
+
+  })
+
+
+}
+
+function getTableHeadingMarkdown(lines) {
+  return Array(lines.at(-1).split("|").length)
+          .fill("---|")
+          .join("");
 }
 
 
