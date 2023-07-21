c82decf519c661300cbfca8f42e7fc3f3f6ef813
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-07-21 18:33:42 +0200

    (github-actions): refactor getStatsFromEntwicklerHeld for better readability

--- .github/scripts/getStatsFromEntwicklerHeld.js
+++ .github/scripts/getStatsFromEntwicklerHeld.js
@@ -1,44 +1,79 @@
 import challengeData from "./challengeData.json" assert { type: "json" }
 
-async function getStatsFromEntwicklerHeld(challengeId, challengeUrl) {
+async function getStatsFromEntwicklerHeld() {
 
-  const complexityDict = ["", "Easy", "Medium", "Hard", "Hardcore"]
+  const processedData = await processData(challengeData);
+  const overviewMarkdown = createOverviewMarkdown(processedData);
+  console.log(processedData, JSON.stringify(processedData), overviewMarkdown)
+
+}
+
+
+async function getDataFromApi(idEH) {
+
+  try {
+    const fetchResponse = await fetch(`https://tasks.platform.entwicklerheld.de/api/campaign/${idEH}/details/`, {
+      headers: {
+        "Origin": "https://platform.entwicklerheld.de"
+      }
+    });
+
+    const { title, slug, complexity, statistics, technology } = await fetchResponse.json();
+
+    //validate expected types
+    const isExpectedStrings = [title, slug, technology.language].every(entry => typeof(entry) === "string" && entry.length > 0);
+    const isExpectedNumbers = [complexity, statistics.solutionsCount, statistics.acceptancesCount].every(entry => typeof(entry) === "number");
+
+    if (isExpectedStrings === false || isExpectedNumbers === false) {
+      throw new Error("Api Data Validation Failed. Received back unexpected data");
+    };
+
+    return {
+      title,
+      slug,
+      complexity,
+      statistics,
+      language: technology.language
+    }
+  }
+  catch(err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+async function processData(challengeData) {
+
+  const complexityDict = ["Easy", "Medium", "Hard", "Hardcore"];
 
   const processedData = await Promise.all(
     challengeData.challengeData.map( async entry => {
 
-      const fetchResponse = await fetch(`https://tasks.platform.entwicklerheld.de/api/campaign/${entry.idEH}/details/`, {
-        headers: {
-          "Origin": "https://platform.entwicklerheld.de",
-          //"Referer": challengeUrl
-        }
-      });
-      const { title, slug, complexity, statistics, technology } = await fetchResponse.json();
+      const { title, slug, complexity, statistics, language } = await getDataFromApi(entry.idEH);
 
-      entry.urlEH = challengeData.baseUrlEH + (entry.urlEH != "") ? entry.urlEH : `${slug}?technology=${technology.language}`;
+      entry.urlEH = challengeData.baseUrlEH + (entry.urlEH != "") ? entry.urlEH : `${slug}?technology=${language}`;
       entry.urlSolution = (entry.urlSolution != "") ? `${challengeData.baseUrlSolution}${entry.urlSolution}` : `${challengeData.baseUrlSolution}${slug}`;
+
       entry.difficulty = {
         complexity,
-        name: complexityDict[complexity],
+        name: complexityDict[complexity-1],
         bar: Array(4).fill("▮", 0, complexity).fill("▯", complexity).join("")
-      }
+      };
 
-      entry.statistics = statistics
+      entry.statistics = statistics;
       entry.statistics.succesRate = Math.round(statistics.solutionsCount / (statistics.acceptancesCount / 100));
 
       const succesRateRounded = Math.round(entry.statistics.succesRate/10);
       entry.statistics.succesRateBar = Array(10).fill("█", 0, succesRateRounded).fill("░", succesRateRounded).join("");
 
-      entry.technology = technology.language;
+      entry.language = language;
       entry.title = title;
 
       return entry;
     })
   )
 
-  const markdown = writeOverviewMarkDown(processedData);
-  console.log(processedData, JSON.stringify(processedData), markdown)
-
+  return processedData
 }
 
 
