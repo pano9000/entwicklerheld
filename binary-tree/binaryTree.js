db53b6d339b420dc3e1947f4c7b5103ae6cec22c
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-07-27 09:19:11 +0200

    (binaryTree): final clean up

--- binary-tree/binaryTree.js
+++ binary-tree/binaryTree.js
@@ -1,31 +1,22 @@
-import { TreeNode } from "./tree.js"
 import 'regenerator-runtime/runtime' //required due to Babel
 
-export function printTree(treeNode) {
 
-    const result = [];
-    const traverseTree = traverseTreeBFS(treeNode);
-
-    while (true) {
-        const currentNode = traverseTree.next();
-        if (currentNode.done) break;
-        const currentDepth = currentNode.value.depth;
-        if (result[currentDepth] === undefined) { result[currentDepth] = [] };
-        result[currentDepth].push(currentNode.value.value)
-    }
-    console.log(result)
-    return result.join("\n");
-
-}
-
-
-function* traverseTreeBFS(treeNode) {
+function* getBFSTreeTraversalGenerator(treeNode, options = { reverse: false }) {
     const queue = [treeNode];
     let depth = 0;
     let tempDepth = 1;
     while (queue.length > 0) {
         const currentNode = queue.shift();
-        yield {value: currentNode.value, depth }
+
+        if (options.reverse === true) {
+            if (currentNode !== null) {
+                let tempNode = currentNode.left;
+                currentNode.left = currentNode.right;
+                currentNode.right = tempNode;
+            }
+        }
+
+        yield { currentNode, depth }
 
         if (currentNode.left !== null) {
             queue.push(currentNode.left)
@@ -43,75 +34,47 @@ function* traverseTreeBFS(treeNode) {
             tempDepth++;
         }
     }
+    return treeNode;
 }
 
-function* mirrorTree(t) {
 
-    //I need to work with a queue here it seems
-    let tree = t;
-    let count = 0;
-    //let treeDir = 
-    //if (count == 0)
-    current
-    while(true) {
-        if (count === 1) {
-            console.log("in 1 yielllld")
+export function printTree(treeNode) {
 
-            count = 0;
-            console.log("mirrrtrrr", tree, "yieeeeld")
-            tree.right = yield;
-        } else {
-            count++;
-            console.log("mirrrtrrr", tree, "yieeeeld")
-            tree.left = yield;
-        }
-        console.log("after else yielllld", new Date())
+    const result = [];
+    const treeTraversalGen = getBFSTreeTraversalGenerator(treeNode);
+
+    while (true) {
+        const currentNode = treeTraversalGen.next();
+        if (currentNode.done) break;
+        const currentDepth = currentNode.value.depth;
+        if (result[currentDepth] === undefined) { result[currentDepth] = [] };
+        result[currentDepth].push(currentNode.value.currentNode.value)
     }
+    return result.join("\n");
+
 }
+
 
 // Step through each node and check if same or not -> exits early, if there is a difference
 export function checkTreeEqualness(treeNode1, treeNode2){
 
-    const treeGenerators = [traverseTreeBFS(treeNode1), traverseTreeBFS(treeNode2)];
+    const treeTraversalGens = [getBFSTreeTraversalGenerator(treeNode1), getBFSTreeTraversalGenerator(treeNode2)];
     
     while(true){
-        const currentSteps = [treeGenerators[0].next(), treeGenerators[1].next()];
+        const currentSteps = [treeTraversalGens[0].next(), treeTraversalGens[1].next()];
         if (currentSteps[0].done && currentSteps[1].done) return true;
-        if (currentSteps[0].value.value !== currentSteps[1].value.value) return false;
+        if (currentSteps[0].value.currentNode.value !== currentSteps[1].value.currentNode.value) return false;
     }
 
 }
 
 export function reverse(treeNode) {
 
-    const reversedTreeNode = JSON.parse(JSON.stringify(treeNode));
-    const queue = [reversedTreeNode];
-
-    while (queue.length > 0) {
-
-        const currentNode = queue.shift();
-
-        //shift
-        if (currentNode !== null) {
-            let tempNode = currentNode.left;
-            currentNode.left = currentNode.right;
-            currentNode.right = tempNode;
+    const treeTraversalGen = getBFSTreeTraversalGenerator(treeNode, {reverse: true});
+    while (true) {
+        const currentNode = treeTraversalGen.next();
+        if (currentNode.done) {
+            return currentNode.value;
         }
-        //
-
-        if (currentNode.left !== null) {
-            queue.push(currentNode.left)
-        }
-
-        if (currentNode.right !== null) {
-            queue.push(currentNode.right)
-        }
-
-
     }
-
-    console.log("reverse", printTree(reversedTreeNode));
-
-    return reversedTreeNode
-
 }
