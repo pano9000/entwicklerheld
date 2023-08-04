01959e7d892e4a89d704e0681b6f2a7633d9ed62
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-08-04 09:06:23 +0200

    (rest-api): add final solution

--- rest-api/IOUApi.java
+++ rest-api/IOUApi.java
@@ -4,92 +4,85 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.net.URI;
+
 
 public class IOUApi {
 
     private static IOUProvider iouProvider = IOUProvider.getInstance();
-    //private static ApiEndpoint[] apiEndpoints = {new ApiEndpoint("/ious"), new ApiEndpoint("/ious/:id")};
 
-
-    private static void getPathParameter(HttpRequest request) {
-
+    private static String[] getPathFromUri(String path) {
         try {
-            URI uri = new URI(request.getPath());
-            System.out.println("uripath // " +uri.getPath().split("/"));
-            List<String> a = new ArrayList<>();
+            //get rid of first forwardslash for easier processing later on
+            String uriPath = new URI(path).getPath().substring(1); 
+            return uriPath.split("/");
         } catch (Exception e) {
-            // TODO: handle exception
+            System.out.println(e.getMessage());
+            return null;
         }
-
     }
 
     public static Response<JSONArray> handleRequest(HttpRequest request) {
-        System.out.println(""+request.getMethod() + " / " + request.getPath() + " / " + request.getBody() + "\n");
-        String[] pathParts = request.getPath().split("/");
-        getPathParameter(request);
 
-        try {
-            URI uri = new URI(request.getPath());
-            System.out.println("uripath // " +uri.getPath());
-        } catch (Exception e) {
-            // TODO: handle exception
+        Integer iouId = null;
+        String[] pathParts = getPathFromUri(request.getPath());
+        if (pathParts == null) return new Response<JSONArray>(500);
+        if (pathParts[0].equals("ious") == false) return new Response<JSONArray>(404);
+
+        if (pathParts.length > 1) {
+            try {
+                //this should rather be done in separate validation stage before already
+                iouId = Integer.parseInt(pathParts[1]); 
+            } catch(Exception e) {
+                return new Response<JSONArray>(400);
+            }
         }
 
-       /*  if (pathParts[3] != "ious") {
-            return new Response<>(404);
-        }*/
-
         switch (request.getMethod()) {
-            case "GET":
-                return handleGet(request);
+            case "GET":                
+                return handleGet(request, iouId);
             case "POST":
                 return handlePost(request);
             case "PATCH":
-                return handlePatch(request);
+                return handlePatch(request, iouId);
             case "DELETE":
-                return handleDelete(request);
+                return handleDelete(request, iouId);
             default:
-                return null;
+                return new Response<JSONArray>(500);
         }
 
     };
 
-    private static Response handleGet(HttpRequest request) {
+    private static Response<JSONArray> handleGet(HttpRequest request, Integer iouId) {
 
-        String[] pathParts = request.getPath().split("ious/");
-        System.out.println("handleGET" + " / " + pathParts.length);
-        
-        if (pathParts.length == 1) {
+        JSONArray resBody;
+        if (iouId == null) {
             List<IOU> ious = iouProvider.getIOUs();
-            JSONArray body = new JSONArray(ious);
-            Response<JSONArray> response = new Response<JSONArray>(200, body);
-            return response;
+            resBody = new JSONArray(ious);
         } else {
-            IOU iou = iouProvider.getIOU(Integer.parseInt(pathParts[1]));
-            if (iou == null) {
-                Response response = new Response(404);
-                return response;
-            }
+            IOU iou = iouProvider.getIOU(iouId);
+            if (iou == null) return new Response<JSONArray>(404);
             JSONObject iouJSON = new JSONObject(iou);
-            JSONArray resBody = new JSONArray();
+            resBody = new JSONArray();
             resBody.put(iouJSON);
-            return new Response(200, resBody);
         }
-            
-
+        
+        return new Response<JSONArray>(200, resBody);
 
     }
 
+
     private static Response<JSONArray> handlePost(HttpRequest request) {
 
-        //do some body validation here
+        //in real world you need to do some body validation here using some library,
+        // ideally with a pre-defined scheme of the expected/allowed JSON data 
 
         try {
             JSONObject reqBody = new JSONObject(request.getBody());
-            
             JSONObject debtorJ = reqBody.getJSONObject("debtor");
             JSONObject creditorJ = reqBody.getJSONObject("creditor");
             
@@ -116,56 +109,39 @@ public class IOUApi {
             JSONObject iouJSON = new JSONObject(iou);
             JSONArray resBody = new JSONArray();
             resBody.put(iouJSON);
-            return new Response(201, resBody);
+            return new Response<JSONArray>(201, resBody);
             
         } catch (Exception e) {
             System.out.println(e);
-            return new Response(500);
+            return new Response<JSONArray>(500);
         }
 
     }
 
-    private static Response<JSONArray> handleDelete(HttpRequest request) {
+    private static Response<JSONArray> handleDelete(HttpRequest request, Integer iouId) {
+        if (iouId == null) return new Response<JSONArray>(400);
 
-        String[] pathParts = request.getPath().split("ious/");
-        int statusCode;
-
-        if (pathParts.length == 1) {
-
-            statusCode = 400;
-
-        } else {
-            iouProvider.removeIOU(iouProvider.getIOU(Integer.parseInt(pathParts[1])));
-            statusCode = 204;
-        }
+        iouProvider.removeIOU(iouProvider.getIOU(iouId));
         
-        Response<JSONArray> response = new Response<JSONArray>(statusCode);
+        Response<JSONArray> response = new Response<JSONArray>(204);
         return response;
 
     }
 
-    private static Response<JSONArray> handlePatch(HttpRequest request) {
-        String[] pathParts = request.getPath().split("ious/");
-        Integer iouId = (pathParts.length == 1) ? null : Integer.parseInt(pathParts[1]);
+    private static Response<JSONArray> handlePatch(HttpRequest request, Integer iouId) {
 
-        if (iouId == null) {
-            return new Response<JSONArray>(400);
-        }
-
+        if (iouId == null) return new Response<JSONArray>(400);
         IOU iou = iouProvider.getIOU(iouId);
 
-        if (iou == null) {
-            return new Response<JSONArray>(404);
-        }
+        if (iou == null) return new Response<JSONArray>(404);
 
-        System.out.println("patchyy // " + request.getBody());
         JSONObject reqBody;
         JSONArray resBody = new JSONArray();
+
         try {
             reqBody = new JSONObject(request.getBody());
             String[] attributeKeys = JSONObject.getNames(reqBody);
             for (String key : attributeKeys) {
-                System.out.println("getNamessss // " + key);
                 switch (key) {
                     case "amount":
                         iou.setAmount(reqBody.getDouble(key));
@@ -197,26 +173,12 @@ public class IOUApi {
             
         } catch (Exception e) {
             // TODO: handle exception
-            return new Response(500);
+            return new Response<JSONArray>(500);
         }
         
         resBody.put(new JSONObject(iou));
-        return new Response(200, resBody);
+        return new Response<JSONArray>(200, resBody);
 
-    }
-
-/*
-    class Router {
-        String[] pathParts = request.getPath().split("/");
-        System.out.println("pathp" + " / " + pathParts.length + "// " + pathParts[3] );
-    }
- */
-
-    class ApiEndpoint {
-        private final String endpointName;
-        public ApiEndpoint(String endpointName) {
-            this.endpointName = endpointName;
-        }
     }
 
 }
