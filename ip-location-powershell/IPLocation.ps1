7b23cd7155b8154849956582e4094b6c49df64e2
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-10-09 08:40:37 +0200

    (ip-location-powershell): add my solution

--- ip-location-powershell/IPLocation.ps1
+++ ip-location-powershell/IPLocation.ps1
@@ -1,3 +1,33 @@
 Function IPLocation ([string] $FilePath, [string] $OutputPath, [string] $APIKey) {
-    # Implement here
+
+    $fileContent = (Get-Content -Path $FilePath);
+
+    if ($null -eq $fileContent) {
+        throw "File is empty";
+    }
+
+    $ipList = $fileContent.Split([Environment]::NewLine);
+
+    $results = New-Object System.Collections.ArrayList;
+    foreach ($rawIp in $ipList) {
+
+        try { 
+            $ip = [IPAddress] $rawIp
+        } catch {
+            throw "IP Address ${rawIp} is invalid."
+        }
+
+        $reqBody = @{
+            ip = $ip;
+            key = $APIKey;
+        };
+        $timestamp = Get-Date -Format "dd.MM.yyyy HH:mm"
+
+        $response = Invoke-RestMethod -Uri "https://api.ip2location.io" -Method Get -Body $reqBody;
+        $result = $response | Select-Object -Property ip, country_code, as, is_proxy, @{Name = "query_date_and_time"; Expression = {$timestamp}}
+        $results.Add($result);
+    }
+
+    $sortedResults = $results |Sort-Object -Property { [System.Version]$_.ip };
+    $sortedResults | Export-Csv  -Path $OutputPath -NoTypeInformation;
 }
