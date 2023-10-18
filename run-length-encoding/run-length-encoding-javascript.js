deaf9f766cae7f35efb1973ff385a007f57f7594
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-10-19 00:42:12 +0200

    (run-length-encoding): add initial solution

--- run-length-encoding/run-length-encoding-javascript.js
+++ run-length-encoding/run-length-encoding-javascript.js
@@ -1,4 +1,24 @@
+/**
+ * Return a Run Length Encoded version of the string
+ * @param {string} text 
+ * @returns {string} Run Length Encoded String
+ */
 export function encode(text) {
-  // implement this
-   return "";
+  const encodedParts = [];
+
+  let currentEnc = [0, text[0]];
+  for (let i = 0; i < text.length; i++) {
+    if (currentEnc[1] !== text[i]) {
+      encodedParts.push(currentEnc.join(""));
+      currentEnc = [0, text[i]];
+    }
+
+    currentEnc[0]++;
+
+    if (i === text.length-1) {
+      encodedParts.push(currentEnc.join(""));
+    }
+  }
+
+  return encodedParts.join("");
 }
