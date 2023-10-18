8bfc07b2ab9bc9973e04411dbe64b8d2c3aec875
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-10-19 00:33:29 +0200

    (run-length-encoding): adjust tests to personal preference

--- run-length-encoding/run-length-encoding-javascript.test.js
+++ run-length-encoding/run-length-encoding-javascript.test.js
@@ -1,35 +1,38 @@
-import { encode } from "./run-length-encoding-javascript";
+import { encode } from "./run-length-encoding-javascript.js";
+import { expect } from "chai";
 
-beforeEach(() => {
-      console.log("##polylith[testStarted");
-});
+const testCasesEnc = [
+  ["aaa", "3a"],
+  ["", ""],
+  ["aab", "2a1b"],
+  ["a", "1a"],
+  ["aba", "1a1b1a"],
+  ["abc", "1a1b1c"],
+  ["aaabbc", "3a2b1c"],
+  ["aaabbaaa", "3a2b3a"],
+  ["aaabaaa", "3a1b3a"],
+  ["aaabbbaaa", "3a3b3a"],
+  ["aAa", "1a1A1a"],
+  ["aAaAaA", "1a1A1a1A1a1A"],
+  ["aAaAaAa", "1a1A1a1A1a1A1a"],
+  ["aAaAaAaA", "1a1A1a1A1a1A1a1A"],
+  ["aaaaaaaaaabbbb", "10a4b"],
+  ["aAaAaAaAa", "1a1A1a1A1a1A1a1A1a"],
+  ["aaabaaa", "3a1b3a"],
+]
 
-afterEach(() => {
-      console.log("##polylith[testFinished")
-});
+describe("#Encode", () => {
 
-test('first scenario', () => {
-    check("aaa", "3a");
-    check("", "");
-    check("aab", "2a1b");
-    check("a", "1a");
-    check("aba", "1a1b1a");
-    check("abc", "1a1b1c");
-    check("aaabbc", "3a2b1c");
-    check("aaabbaaa", "3a2b3a");
-    check("aaabaaa", "3a1b3a");
-    check("aaabbbaaa", "3a3b3a");
+  testCasesEnc.forEach(testCase => {
+    getTestCheckEnc(testCase);
+  })
+})
 
-    check("aAa", "1a1A1a");
-    check("aAaAaA", "1a1A1a1A1a1A");
-    check("aAaAaAa", "1a1A1a1A1a1A1a");
-    check("aAaAaAaA", "1a1A1a1A1a1A1a1A");
-    check("aaaaaaaaaabbbb", "10a4b");
-    check("aAaAaAaAa", "1a1A1a1A1a1A1a1A1a");
-    check("aaabaaa", "3a1b3a");
-});
-
-function check(text, encoded_text) {
-      let result = encode(text);
-      expect(result, "Expected '" + encoded_text + "' for text '" + text + "' but was " + result).toBe(encoded_text);
+function getTestCheckEnc(testCase) {
+  return it(`Case: '${testCase.join(", ")}'`, () => {
+    const [text, encoded_text] = testCase;
+    const result = encode(text);
+    expect(result, `Expected '${encoded_text}' for text '${text}' but was '${result}'`).to.eql(encoded_text);
+  })
 }
+
