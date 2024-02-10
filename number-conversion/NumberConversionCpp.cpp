3f30e0b32eabf965dd08d2b7e7ced0c42d847d2e
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-02-10 23:17:10 +0100

    (number-conversion): add solution for first function

--- number-conversion/NumberConversionCpp.cpp
+++ number-conversion/NumberConversionCpp.cpp
@@ -1,9 +1,34 @@
 #include "NumberConversionCpp.hpp"
+#include <iostream>
+#include <cmath>
+#include <string>
 
 std::string toBin(uint32_t decimalNumber)
 {
-	// Implement
-	return "";
+	// Edge case of 0
+	if (decimalNumber == 0)
+	{
+		return "0";
+	}
+
+	uint32_t exponent = std::floor(log2(decimalNumber));
+	std::string binaryString = "";
+
+	for (int i = exponent; i >= 0; i--)
+	{
+		int pow_result = std::pow(2, i);
+
+		if (pow_result > decimalNumber)
+		{
+			binaryString += "0";
+		} 
+		else 
+		{
+			decimalNumber -= pow_result;
+			binaryString += "1";
+		}
+	}
+	return binaryString;
 }
 
 std::string toHex(uint32_t decimalNumber)
