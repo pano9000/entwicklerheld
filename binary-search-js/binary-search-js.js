cc393c624276eace6117e8d519a5088b065ef457
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-02-18 08:23:09 +0100

    (binary-search-js): add initial state and test

---
+++ binary-search-js/binary-search-js.js
@@ -0,0 +1,6 @@
+import { Result } from "./result";
+
+export const find = (array, element) => {
+    // You have to implement this method
+    throw new Error('Not implemented');    
+};
---
+++ binary-search-js/result.js
@@ -0,0 +1,6 @@
+export class Result {
+    constructor(step, index) {
+        this.step = step;
+        this.index = index;
+    }
+}
