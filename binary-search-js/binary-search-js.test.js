f788d151e445c84d0d8dc95d25d362e1d1f5446b
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-02-29 19:21:16 +0100

    (binary-search-js): make tests a bit less "DRY"

--- binary-search-js/binary-search-js.test.js
+++ binary-search-js/binary-search-js.test.js
@@ -2,66 +2,66 @@ import { find } from "./binary-search-js.js";
 import { Result } from "./result.js";
 import { expect } from "chai";
 
-it('test 1 1', () => {
-    let arr = [6];
-    let element = 6;
-    let actual = find(arr, element);
-    let expected = new Result(0, 0);
-    expect(actual, `Given an array [ ${arr} ] with one element the element ${element} should be found in step ${expected.step} an return index ${expected.index}.`).to.deep.equal(expected);
-});
+const testCases = [
+  [
+    'test 1 1',
+    [[6], 6],
+    new Result(0, 0)
+  ],
 
-it('test 1 2', () => {
-    let arr = [1, 3, 4, 6, 8, 9, 11];
-    let element = 6;
-    let actual = find(arr, element);
-    let expected = new Result(0, 3);
-    expect(actual, `Given an array [ ${arr} ] with ${arr.length} elements the element ${element} should be found in step ${expected.step} and return index ${expected.index}.`).to.deep.equal(expected);
-});
+  [
+    'test 1 2',
+    [[1, 3, 4, 6, 8, 9, 11], 6],
+    new Result(0, 3)
+  ],
 
-it('test 1 3', () => {
-    let arr = [1, 3, 4, 6, 8, 9, 11];
-    let element = 1;
-    let actual = find(arr, element);
-    let expected = new Result(2, 0);
-    expect(actual, `Given an array [ ${arr} ] with ${arr.length} elements the element ${element} should be found in step ${expected.step} and return index ${expected.index}.`).to.deep.equal(expected);
-});
+  [
+    'test 1 3',
+    [[1, 3, 4, 6, 8, 9, 11], 1],
+    new Result(2, 0)
+  ],
 
-it('test 1 4', () => {
-    let arr = [1, 3, 4, 6, 8, 9, 11];
-    let element = 11;
-    let actual = find(arr, element);
-    let expected = new Result(2, 6);
-    expect(actual, `Given an array [ ${arr} ] with ${arr.length} elements the element ${element} should be found in step ${expected.step} and return index ${expected.index}.`).to.deep.equal(expected);
-});
+  [
+    'test 1 4',
+    [[1, 3, 4, 6, 8, 9, 11], 11],
+    new Result(2, 6)
+  ],
 
-it('test 1 5', () => {
-    let arr = [1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377];
-    let element = 21;
-    let actual = find(arr, element);
-    let expected = new Result(0, 5);
-    expect(actual, `Given an array [ ${arr} ] with ${arr.length} elements the element ${element} should be found in step ${expected.step} and return index ${expected.index}.`).to.deep.equal(expected);
-});
+  [
+    'test 1 5',
+    [[1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377], 21],
+    new Result(0, 5)
+  ],
 
-it('test 1 6', () => {
-    let arr = [1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634];
-    let element = 144;
-    let actual = find(arr, element);
-    let expected = new Result(1, 9);
-    expect(actual, `Given an array [ ${arr} ] with ${arr.length} elements the element ${element} should be found in step ${expected.step} and return index ${expected.index}.`).to.deep.equal(expected);
-});
+  [
+    'test 1 6',
+    [[1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634], 144],
+    new Result(1, 9)
+  ],
 
-it('test 2 1', () => {
-    let arr = [];
-    let element = 2;
-    let actual = find(arr, element);
-    let expected = new Result(0, -1);
-    expect(actual, `Given an array [ ${arr} ] with ${arr.length} elements the element ${element} should be found in step ${expected.step} and return index ${expected.index}.`).to.deep.equal(expected);
-});
+  [
+    'test 2 1',
+    [[], 2],
+    new Result(0, -1)
+  ],
 
-it('test 2 2', () => {
-    let arr = [1, 3, 4, 6, 8, 9, 11];
-    let element = 2;
+  [
+    'test 2 2',
+    [[1, 3, 4, 6, 8, 9, 11], 2],
+    new Result(3, -1)
+  ]
+]
+
+const testFunc = (testCase) => {
+  return it(testCase[0], () => {
+    let arr = testCase[1][0];
+    let element = testCase[1][1];
     let actual = find(arr, element);
-    let expected = new Result(3, -1);
+    let expected = testCase[2];
     expect(actual, `Given an array [ ${arr} ] with ${arr.length} elements the element ${element} should be found in step ${expected.step} and return index ${expected.index}.`).to.deep.equal(expected);
+  })
+}
+
+describe("Binary Search Tests", () => {
+  testCases.forEach(testCase => testFunc(testCase));
 });
