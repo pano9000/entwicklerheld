2362f87a95612256c52d67b36d1c4b48172c9846
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-02-29 19:00:36 +0100

    (binary-search-js): add my solution

--- binary-search-js/binary-search-js.js
+++ binary-search-js/binary-search-js.js
@@ -1,6 +1,35 @@
-import { Result } from "./result";
+import { Result } from "./result.js";
 
 export const find = (array, element) => {
-    // You have to implement this method
-    throw new Error('Not implemented');    
+
+  let step = 0;
+
+  const searchRange = {
+    start: 0,
+    end: array.length - 1,
+    remainingElements: array.length
+  };
+
+  while (searchRange.remainingElements > 0) {
+
+    let indexMiddle = Math.floor((searchRange.remainingElements - 1) / 2);
+    let currentValueIndex = indexMiddle + searchRange.start;
+
+    if (array[currentValueIndex] === element) {
+      return new Result(step, currentValueIndex);
+    }
+
+    if (array[currentValueIndex] > element) {
+      searchRange.end = currentValueIndex - 1;
+    }
+    else {
+      searchRange.start = currentValueIndex + 1;
+    }
+
+    searchRange.remainingElements = searchRange.end - searchRange.start + 1;
+    step++;
+  }
+
+  return new Result(step, -1)
+
 };
