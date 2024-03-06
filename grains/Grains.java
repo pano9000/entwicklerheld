d7b4edb59ac8bfc37c3abb0ca5dff18b42ffda23
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-03-06 22:01:59 +0100

    (grains): add my solution

--- grains/Grains.java
+++ grains/Grains.java
@@ -1,17 +1,19 @@
 package de.entwicklerheld.grainsJava;
 
-
 import java.math.BigInteger;
 
 public class Grains {
+
     BigInteger grainsOnSquare(final int square) {
-        // implement this
-        return null;
+
+        if (!(square >= 1 && square <= 64)) {
+            throw new IllegalArgumentException("square must be between 1 and 64");
+        }
+        return new BigInteger("2").pow(square - 1);
     }
 
     BigInteger grainsOnBoard() {
-        // implement this
-        return null;
+        return new BigInteger("2").pow(64).subtract(new BigInteger("1"));
     }
 
 
