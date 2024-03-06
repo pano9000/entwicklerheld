abad369fec2d21a3e2af8c661dacc0595c32bb63
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-03-06 19:38:05 +0100

    (grains): add initial state

---
+++ grains/Grains.java
@@ -0,0 +1,18 @@
+package de.entwicklerheld.grainsJava;
+
+
+import java.math.BigInteger;
+
+public class Grains {
+    BigInteger grainsOnSquare(final int square) {
+        // implement this
+        return null;
+    }
+
+    BigInteger grainsOnBoard() {
+        // implement this
+        return null;
+    }
+
+
+}
