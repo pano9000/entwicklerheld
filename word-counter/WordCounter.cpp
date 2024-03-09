0da1c38a063aad2abf899bb0260497ad69697b8c
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-03-09 17:59:03 +0100

    (word-counter): add my solution

--- word-counter/WordCounter.cpp
+++ word-counter/WordCounter.cpp
@@ -1,19 +1,78 @@
 #include "WordCounter.hpp"
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include <cstdlib>
+
 string WordCounter::read_file(const string &filepath)
 {
-    // Implement this for Scenario 1
-    throw std::runtime_error("Not Implemented");
+   std::ifstream file (filepath, std::ios::binary);
+    if (!file.is_open())
+    {
+        throw std::runtime_error("Could not open the file, aborting");
+    }
+
+    ostringstream docstream;
+    docstream << file.rdbuf();
+    this->document = docstream.str();
+    file >> this->document;
+    
+    // reset words_counted
+    map<string, int> words_counted;
+    this->words_counted = words_counted;
+    
+    return this->document;
 }
 
 map<string, int> WordCounter::count_words()
 {
-    // Implement this for Scenario 2
-    throw std::runtime_error("Not Implemented");
+
+    std::string current_word;
+
+    //force a newline at the end to the last word, even if the string ends on a regular character
+    for (char current_char : this->getDocument() + "\n") 
+    {
+
+        if (!(std::isspace(current_char) || std::ispunct(current_char)))
+        {
+            current_word += current_char;
+        }
+
+        else 
+        {
+            if (!(current_word.length() > 0)) continue;
+            
+            if (this->words_counted.count(current_word) == 0)
+            {
+                this->words_counted[current_word] = 0;    
+            }
+            this->words_counted[current_word]++;
+
+            current_word = "";
+        }
+    }
+
+
+    return this->words_counted;
+
 }
 
 string WordCounter::format_output()
 {
-    // Implement this for Scenario 3
-    throw std::runtime_error("Not Implemented");
+    int totalWords = 0;
+    std::stringstream ss;
+
+    for (const auto& entry : this->getWordsCounted())
+    {
+        ss << "\t" << entry.first << ": " << entry.second << ",\n";
+        totalWords += entry.second;
+    }
+
+    ss << "--------------------------\nTotal Words: " << totalWords;
+    //personally I would have added a newline at the end, but this makes the tests fail...
+
+    return ss.str();
 }
