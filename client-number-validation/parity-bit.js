aaaf4c55777f82c656721d725f45ef292cb8be01
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-03-13 00:31:47 +0100

    (client-number-validation): add my solution

--- client-number-validation/parity-bit.js
+++ client-number-validation/parity-bit.js
@@ -1,4 +1,15 @@
+export function validateClientNumber(clientNumber){
+  const formatRegex = /^\d{10}$/;
 
-export function validateClientNumber(clientNumber){
-   return null;
+  if (!formatRegex.test(clientNumber)) {
+     return false;
+  }
+
+  // base2 aka binary
+  const clientNumberAsBase2 = Number(clientNumber).toString(2);
+
+  // count all "1"s in the string
+  const parity = clientNumberAsBase2.split("").reduce( (accum, curr) => (curr == "1") ? ++accum : accum);
+
+  return parity % 2 == 0;
 }
