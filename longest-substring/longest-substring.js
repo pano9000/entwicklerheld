54d16c2fe9cf0fb7df704272c891e2c4c6bc2084
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-03-12 23:36:25 +0100

    (longest-substring): add my solution

--- longest-substring/longest-substring.js
+++ longest-substring/longest-substring.js
@@ -1,4 +1,39 @@
-export function findLongestSubstring() {
-  // You have to implement this method
-  return null;
+export function findLongestSubstring(string1, string2) {
+
+  let longestSubstring = {
+      substrings: [],
+
+      get currLength() {
+          return this.substrings[0]?.length || 0
+      },
+
+      add(substring) {
+          if (substring.length > this.currLength) {
+              this.substrings = [substring]
+          } else {
+              this.substrings.push(substring)
+          }
+      },
+
+      toString() {
+          return this.substrings.join(" ")
+      }
+  }
+
+  for (let i = 0; i < string1.length + 1; i++) {
+
+      for (let j = 1; j < string1.length + 1; j++) {
+          const substr = string1.slice(i, j);
+
+          // if the substring is not >= the current longest string -> skip
+          if (!(substr.length >= longestSubstring.currLength) || !string2.includes(substr)) {
+              continue;
+          }
+
+          longestSubstring.add(substr);
+      }
+
+  }
+
+  return longestSubstring.toString();
 }
