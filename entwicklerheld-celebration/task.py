5483f037bfe9a34182aab442a2a6789ba4ef7b9c
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-03-18 22:03:04 +0100

    (entwicklerheld-celebration): add my solution

--- entwicklerheld-celebration/task.py
+++ entwicklerheld-celebration/task.py
@@ -1,10 +1,11 @@
 def count_clinking_glasses_for_eh_team(number_of_guests):
-    # You need to implement this method.
-    # You can also add attributes to the classes and add new methods or functions
-    raise NotImplementedError
+    # x guests, clinking with everyone else, except themselves -> x -1
+    # and since we are supposed to count clinks, not glasses -> divide by two
+    return number_of_guests * (number_of_guests - 1) / 2
 
 
 def count_clinking_glasses_for_variable_number_of_guests(number_of_guests):
-    # You need to implement this method.
-    # You can also add attributes to the classes and add new methods or functions
-    raise NotImplementedError
+    count = 0
+    for i in range(number_of_guests, 1, -1):
+        count += count_clinking_glasses_for_eh_team(i)
+    return count
