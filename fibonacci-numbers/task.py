98450ab639070aec3b486232207bd74396b6ceb0
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-03-26 21:09:50 +0100

    (fibonacci-numbers): add my solution with memoization

--- fibonacci-numbers/task.py
+++ fibonacci-numbers/task.py
@@ -1,6 +1,19 @@
+from typing import Dict
+
 class FibonacciPython:
 
     @staticmethod
-    def fibonacci(number: int) -> int:
-        # implement me
-        pass
+    def fibonacci(number: int, memo: Dict[int, int] = {1: 0, 2: 1}) -> int:
+
+        if not memo.get(number) == None:
+            return memo.get(number)
+
+        result_a = FibonacciPython.fibonacci(number - 1, memo)
+        result_b = FibonacciPython.fibonacci(number - 2, memo)
+
+        memo.update({
+            number - 1: result_a,
+            number - 2: result_b
+        })
+
+        return result_a + result_b
