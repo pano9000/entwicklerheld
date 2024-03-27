91fbdf3c1a96a20ef58ff6046eb998e8a9116aaa
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-03-28 00:06:51 +0100

    (ancient-encryption): add initial state from EntwicklerHeld

---
+++ ancient-encryption/task.py
@@ -0,0 +1,10 @@
+class VigenereCipher(object):
+
+	def __init__(self, alphabet: str, password: str):
+		pass
+
+	def encode(self, message: str):
+		pass
+		
+	def decode(self, message: str):
+		pass
