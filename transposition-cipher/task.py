30d1fbda81a27383757800a77da656f1528f22eb
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-03-29 00:42:13 +0100

    (transposition-cipher): add initial state by EntwicklerHeld

---
+++ transposition-cipher/task.py
@@ -0,0 +1,10 @@
+class TranspositionCipher:
+
+    def encrypt(self, plain_text: str, rails: int) -> str:
+        # Implement this in test scenario 1 and 2
+        pass
+
+    def decrypt(self, cipher: str, rails: int) -> str:
+        # Implement this in test scenario 3 and 4
+        pass
+
