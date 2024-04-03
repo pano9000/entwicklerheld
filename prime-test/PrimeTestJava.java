899c27ededc835c1a8030b9b26be4c1894eb8aa0
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-04-03 22:56:57 +0200

    (prime-test): add my solution

--- prime-test/PrimeTestJava.java
+++ prime-test/PrimeTestJava.java
@@ -1,9 +1,20 @@
 package de.entwicklerheld.primeTestJava;
+import java.lang.Math; 
 
 
 public class PrimeTestJava {
     public static boolean testPrimeNumber(int number) {
-        // Implement this
-        return false;
+
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+
     }
 }
