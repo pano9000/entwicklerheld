30cdd2234d8aa1f92b239dd6f044f933db6298c1
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-04-05 19:47:42 +0200

    (circuit-breaker): add initial solution - all tests passing, but needs refactoring

--- circuit-breaker/circuit-breaker-javascript.js
+++ circuit-breaker/circuit-breaker-javascript.js
@@ -3,6 +3,11 @@ import {CircuitBreakerStates} from "./utils.js";
 export class CircuitBreaker {
     state = CircuitBreakerStates.CLOSED;
     functionThatCouldFail = null;
+
+    currentFailureCount = 0;
+    callCount = 0;
+    failTimeStamp = null;
+
     // default options
     options = {
         failureCount: 3, // if there are 3 failures in the timeWindow => open the circuit breaker
@@ -15,10 +20,104 @@ export class CircuitBreaker {
             this.options = options;
         }
         this.functionThatCouldFail = functionThatCouldFail;
-
     }
+
+    failTimeDiff(currentDateTime) {
+        return (this.failTimeStamp !== null) ? (currentDateTime - this.failTimeStamp) : 0
+    }
+
 
     fire(currentDateTime, ...parameters) {
-        // implement this
+        this.callCount++;
+        console.log()
+        const timeDiff = this.failTimeDiff(currentDateTime);
+        console.log("--- Call #", this.callCount, "currr", currentDateTime, this.state, this.failTimeStamp, this.currentFailureCount, timeDiff)
+        // implement the timing feature
+
+        if (this.state == CircuitBreakerStates.CLOSED) {
+            try {
+                this.functionThatCouldFail(...parameters);
+                console.log("    --- successfully called");
+                //this.failTimeStamp = null;
+            }
+            catch(err) {
+                
+                this.currentFailureCount++;
+                console.log("    --- Failure #", this.currentFailureCount)
+
+                if (this.currentFailureCount === 1) {
+                    console.log("        --- first failure, start failTimeStamp")
+                    this.failTimeStamp = currentDateTime;
+                    return
+                }
+
+                console.log("    --- timedifff", timeDiff);
+
+                if (this.currentFailureCount === this.options.failureCount && timeDiff < this.options.timeWindow) {
+                    console.log(`    --- ${this.options.failureCount} failures within timeWindow: switching to OPEN`)
+                    this.state = CircuitBreakerStates.OPEN;
+                    this.failTimeStamp = currentDateTime;
+
+                    return;
+                }
+
+            }
+        }
+
+        if (this.state == CircuitBreakerStates.HALF_OPEN) {
+            console.log("    +++ half-open state")
+
+            try {
+                this.functionThatCouldFail(...parameters);
+                this.state = CircuitBreakerStates.CLOSED;
+            }
+            catch(err) {
+                //console.error(err);
+                this.state = CircuitBreakerStates.OPEN;
+                this.failTimeStamp = currentDateTime;
+
+            }
+
+        }
+
+
+
+        if (this.state == CircuitBreakerStates.OPEN) {
+            console.log("    +++ open state")
+            const timeDiff = currentDateTime - this.failTimeStamp;
+            console.log("    +++", timeDiff, `resetTimeout: ${this.options.resetTimeout}`)
+
+            if (timeDiff > this.options.resetTimeout) {
+                console.log("    +++", "resetting to halfopen")
+
+                this.state = CircuitBreakerStates.HALF_OPEN;
+                //this.fire(currentDateTime, ...parameters)
+                try {
+                    console.log("       +++ trying to call func in open")
+                    this.functionThatCouldFail(...parameters);
+                    this.state = CircuitBreakerStates.CLOSED;
+                    this.failTimeStamp = null;
+                    this.currentFailureCount = 0;
+                    console.log("       +++ func OK -> switching to CLOSED")
+
+                }
+                catch(err) {
+                    console.log("       +++ calling fucntion in halfopen failed -> back to Open again")
+                    this.state = CircuitBreakerStates.OPEN;
+                    this.failTimeStamp = currentDateTime;
+                }
+                return
+            }
+            else if (timeDiff <= this.options.resetTimeout) {
+                console.log("    +++ stil in timeout")
+
+            }
+            else {
+                console.log("    +++ stil in timeout in else")
+            }
+
+
+        }
     }
+
 }
