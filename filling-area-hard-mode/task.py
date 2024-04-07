860511fcf9f7770017118a3c11a5bd75c4bb4852
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-04-07 17:35:31 +0200

    (filling-area-hard-mode): add final refactored solution with some extra added difficulty by me (ability to fill multiple fields)

--- filling-area-hard-mode/task.py
+++ filling-area-hard-mode/task.py
@@ -1,14 +1,13 @@
-from typing import Dict
+from typing import Dict, Tuple, List, Union
 
 class Area(object):
 
     def __init__(self, map):
         self.map = map
-        self.area_size = (len(self.map), len(self.map[0]))
+        self.area_size = {"x": len(self.map), "y": len(self.map[0])}
 
 
-
-    def get_point(self, x, y):
+    def get_point(self, x: int, y: int) -> int:
         for found_y, row in enumerate(self.map):
             if found_y != y:
                 continue
@@ -17,17 +16,16 @@ class Area(object):
                     return cell
 
 
-    def set_point(self, x, y, value=0):
+    def set_point(self, x: int, y: int, value: int = 0):
         self.map[y][x] = value
 
 
-    def visualize(self, characters: (str, str) = ("▮", "▯"), spacer: str = " ") -> str:
+    def visualize(self, characters: Tuple[str, str] = ("▮", "▯"), spacer: str = " ") -> str:
         text = ""
         for line in self.map:
             for item in line:
-                #item = characters[0] if item == 1 else characters[1]
-                item = characters[0] if item == 1 else characters[1] if item == 0 else "x" #debug test
-                text += item + spacer 
+                item = characters[0] if item == 1 else characters[1]
+                text += item + spacer
             text += "\n"
         return text
 
@@ -40,116 +38,119 @@ class Area(object):
 
 
 class AreaHelper(object):
+
+    movements = {
+        "right":  { "x":  1, "y": 0},
+        "down":   { "x":  0, "y": 1},
+        "left":   { "x": -1, "y": 0},
+        "up":     { "x":  0, "y": -1}
+    }
+
+
     @staticmethod
     def fill_area(area: Area) -> Area:
-        print(area.area_size)
+        #print(area.visualize())
+        points_inside_boxes = AreaHelper.__get_points_inside_boxes(area)
 
-        print(area.visualize())
-        points_inside = AreaHelper.__get_boundary_start(area)
-
-        if len(points_inside) < 1:
+        if len(points_inside_boxes) < 1:
             return area
 
-        for point in points_inside:
-            filled_area = AreaHelper.__do_fill(area, (point["x"], point["y"]))
+        # Also added support for detecting and filling multiple boxes inside an area
+        for point in points_inside_boxes:
+            filled_area = AreaHelper.__do_fill(area, point)
 
         return filled_area
 
     @staticmethod
-    def __do_fill(area: Area, point_in_area: (int, int)) -> Area:
-        x, y = point_in_area
-        gotten_point = area.get_point(x, y)
+    def is_point_inside_area(area: Area, point: Dict[str, int]) -> bool:
+
+      return (point["x"] < area.area_size["y"] and point["x"] >= 0) and \
+               (point["y"] < area.area_size["x"] and point["y"] >= 0)
+
+    @staticmethod
+    def __do_fill(area: Area, point: Dict[str, int]) -> Area:
+
+        gotten_point = area.get_point(point["x"], point["y"])
 
         if gotten_point is None or gotten_point == 1:
             return area
 
-        area.set_point(x, y, 1)
+        area.set_point(point["x"], point["y"], 1)
 
-        moves = {
-            "right": (x + 1, y),
-            "down": (x, y + 1),
-            "left": (x - 1, y),
-            "up": (x, y - 1)
-        }
-
-        for direction in moves:
-            AreaHelper.__do_fill(area, moves[direction])
+        for direction in AreaHelper.movements:
+            next_point = { 
+                "x": point["x"] + AreaHelper.movements[direction]["x"],
+                "y": point["y"] + AreaHelper.movements[direction]["y"]
+            }
+            AreaHelper.__do_fill(area,  next_point)
 
         return area
 
 
-
-    #rename to get_point_inside_boundary or similar
     @staticmethod
-    def __get_boundary_start(area: Area):
+    def __get_points_inside_boxes(area: Area) -> List[Dict[str, int]]:
 
-        possible_points_inside = []
-        for row in range(0, area.area_size[0]):
-          accum_row = AreaHelper.__get_possible_boundary(area=area, curr_point_set = {"x": row, "y": 0} )
-          if accum_row != False:
-            possible_points_inside.append(accum_row)
+        points_inside_boxes = []
+        for key, val in {"row": area.area_size["x"], "col": area.area_size["y"]}.items():
 
-        for col in range(0, area.area_size[1]):
-          accum_col = AreaHelper.__get_possible_boundary(area=area, curr_point_set = {"x": 0, "y": col} )
-          if accum_col != False:
-            possible_points_inside.append(accum_col)
+            for curr_cell in range(0, val):
+                curr_point = {
+                    "x": curr_cell if key == "row" else 0,
+                    "y": curr_cell if key == "col" else 0
+                }
+                result = AreaHelper.__check_diagonally_for_points_inside_boxes(area=area, starting_point=curr_point)
+                if result != False:
+                    points_inside_boxes.append(result)
 
-        return possible_points_inside
+        return points_inside_boxes
 
     @staticmethod
-    def __get_possible_boundary(area: Area, curr_point_set: Dict[str, int] = {"x": int, "y": int}):
+    def __check_diagonally_for_points_inside_boxes(area: Area, starting_point: Dict[str, int]) -> Union[Dict[str, int], bool]:
 
+        curr_point = starting_point
         while True:
 
-            next_point_set = {"x": curr_point_set["x"]+1, "y": curr_point_set["y"]+1 }
+            next_point = {"x": curr_point["x"]+1, "y": curr_point["y"]+1 }
 
             # if next point is out_of_areamap stop and return False
-            if (next_point_set["x"] > area.area_size[1] or next_point_set["x"] < 0) or (next_point_set["y"] > area.area_size[0] or next_point_set["y"] < 0):
+            if AreaHelper.is_point_inside_area(area=area, point=next_point) != True:
                 return False
 
-            curr_area_point = area.get_point(**curr_point_set)
-            next_area_point = area.get_point(**next_point_set)
+            curr_area_point = area.get_point(**curr_point)
+            next_area_point = area.get_point(**next_point)
 
             if curr_area_point != 1 or next_area_point != 0:
-                curr_point_set = next_point_set
+                curr_point = next_point
                 continue
 
             #Check if point is fully enclosed by 1s
-            is_fully_eclosed_result = AreaHelper.__check_if_point_fully_enclosed(area, next_point_set)
+            is_fully_eclosed_result = AreaHelper.__check_if_point_fully_enclosed(area, next_point)
 
             if is_fully_eclosed_result == False:
-                curr_point_set = next_point_set
+                curr_point = next_point
                 continue
 
-            return next_point_set
+            return next_point
 
 
 
 
     @staticmethod
-    def __check_if_point_fully_enclosed(area: Area, point_set: {str, int}) -> bool:
-        #@TODO move to class attribute
-        moves = {
-            "right":  ( 1, 0),
-            "down":   ( 0, 1),
-            "left":   (-1, 0),
-            "up":     ( 0, -1)
-        }
+    def __check_if_point_fully_enclosed(area: Area, point: Dict[str, int]) -> bool:
 
-        for direction in moves:
+        for direction in AreaHelper.movements:
 
-            working_point_set = point_set.copy()
+            working_point = point.copy()
             while True:
-                working_point_set["x"] += moves[direction][0]
-                working_point_set["y"] += moves[direction][1]
+                working_point["x"] += AreaHelper.movements[direction]["x"]
+                working_point["y"] += AreaHelper.movements[direction]["y"]
 
                 # if working_point has run out of values in the area return False, as the working_point is not fully enclosed 
-                if (working_point_set["x"] >= area.area_size[1] or working_point_set["x"] < 0) or \
-                   (working_point_set["y"] >= area.area_size[0] or working_point_set["y"] < 0):
+                if AreaHelper.is_point_inside_area(area=area, point=working_point) != True:
                     return False
 
                 # if point is a boundary: break out of while and continue with next direction
-                if area.get_point(working_point_set["x"], working_point_set["y"]) == 1:
+                if area.get_point(working_point["x"], working_point["y"]) == 1:
                     break
 
         return True
