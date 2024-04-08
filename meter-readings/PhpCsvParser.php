d6cdca1ef00bf7dd7ef5692f99f8cb8df546ed2f
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-04-08 18:54:09 +0200

    (meter-readings): initial state by EH

---
+++ meter-readings/PhpCsvParser.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EntwicklerHeld;
+
+
+class PhpCsvParser extends BaseCsvIterator
+{
+
+    public static function isValid(array $meterReading): bool
+    {
+        // TODO: implement in scenario 1
+        throw new Exception("isValid Method not implemented");
+    }
+
+    public function __construct(string $csvFilePath)
+    {
+        // TODO: implement in scenario 2
+        throw new Exception("__constructor Method not implemented");
+    }
+}
