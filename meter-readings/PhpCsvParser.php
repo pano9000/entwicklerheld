4a7811be31725fcf09d4e0feeedd2d5dd08c5b4a
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-04-08 18:55:20 +0200

    (meter-readings): add stage1 solution

--- meter-readings/PhpCsvParser.php
+++ meter-readings/PhpCsvParser.php
@@ -5,18 +5,53 @@ declare(strict_types=1);
 namespace EntwicklerHeld;
 
 
+use Exception;
+
 class PhpCsvParser extends BaseCsvIterator
 {
+    public $data = array();
 
     public static function isValid(array $meterReading): bool
     {
-        // TODO: implement in scenario 1
-        throw new Exception("isValid Method not implemented");
+
+        $validationChecks = array(
+            isset($meterReading["clientId"]) && !empty($meterReading["clientId"]),
+            isset($meterReading["deletedAt"]) && empty($meterReading["deletedAt"]),
+            isset($meterReading["type"]) && !empty($meterReading["type"]),
+            isset($meterReading["value"]) && is_numeric($meterReading["value"]),
+            isset($meterReading["valid"]) && $meterReading["valid"] == true,
+        );
+
+        $meterReading["valid"] = !in_array(false, $validationChecks);
+
+        return $meterReading["valid"];
     }
 
     public function __construct(string $csvFilePath)
     {
-        // TODO: implement in scenario 2
-        throw new Exception("__constructor Method not implemented");
+
+        $csvFilePointer = fopen($csvFilePath, "r");
+        if (!$csvFilePointer)
+        {
+            throw new Exception("Could not open file.");
+        }
+
+        //TODO should check if csvHeaders are as expected -> TODO in stage2
+        $csvHeaders = fgetcsv($csvFilePointer);
+
+        while ( ($csvLine = fgetcsv($csvFilePointer)) !== false )
+        {
+            $currentLine = array();
+
+            foreach($csvLine as $key=>$value)
+            {
+                $currentLine[$csvHeaders[$key]] = ($csvHeaders[$key] == "valid") ? filter_var($value, FILTER_VALIDATE_BOOLEAN) : $value;
+            }
+
+            array_push($this->data, $currentLine);
+
+        }
+
+        fclose($csvFilePointer);
     }
 }
