741983352fe558db5fb8afc0461c2ecf9a617e76
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-04-15 19:06:29 +0200

    (easy-storage): add initial state by EH

---
+++ easy-storage/EasyStorage.java
@@ -0,0 +1,29 @@
+package de.entwicklerheld.easystorage.challenge.stage1;
+
+import java.util.Map;
+import java.util.Set;
+
+import java.lang.RuntimeException;
+
+class EasyStorage{
+
+    public void store(String item, String repository){
+        //Implement this for scenario 1
+        throw new RuntimeException("store() is not implemented");
+    }
+
+    public Map<String, String> getAllData(){
+        //Implement this for scenario 2
+        throw new RuntimeException("getAllData() is not implemented");
+    }
+
+    public String getRepository(String item){
+        //Implement this for scenario 3
+        throw new RuntimeException("getRepository() is not implemented");
+    }
+
+    public Set<String> getItems(String repository){
+        //Implement this for scenario 3
+        throw new RuntimeException("getItems() is not implemented");
+    }
+}
