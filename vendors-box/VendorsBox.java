f251af8f4b4fa67a1049214a6f936e9371d8f729
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-04-21 18:42:47 +0200

    (vendors-box): clean up initial solution

--- vendors-box/VendorsBox.java
+++ vendors-box/VendorsBox.java
@@ -7,115 +7,102 @@ import java.util.HashMap;
 
 public class VendorsBox {
 
-    private HashMap<String, Double> selectionCount = new HashMap<String, Double>();
+    private HashMap<String, Double> vendorSelectionCount = new HashMap<String, Double>();
     public Vendor findBestVendor(Article article) {
-        // scenario 1
-        // implement this function
-        System.out.println((double)0 + 1);
-        List<Vendor> vendorList = Vendor.getVendorsForArticle(article.getId());
 
-        double lowestPrice = getLowestPriceOfVendors(vendorList, article);
-        List<Vendor> filteredByPrice = vendorsByPrice(vendorList, article, lowestPrice);
+        List<Vendor> vendorsWithLowestPrice = getVendorsWithLowestPrice(article);
 
-        if (filteredByPrice.size() == 1) {
-            return filteredByPrice.get(0);
-        }
-
-        List<Vendor> f = filteredByPrice.stream()
+        List<Vendor> vendorsSortedByCriteria = vendorsWithLowestPrice.stream()
                         .sorted((vendorA, vendorB) -> {
-                            int shippingTimeA = vendorA.getArticleOffer(article.getId()).getShippingTime();
-                            int shippingTimeB = vendorB.getArticleOffer(article.getId()).getShippingTime();
+                            // sort by shipping times
+                            // lower == better
+                            int shippingTimes[] = {
+                                vendorA.getArticleOffer(article.getId()).getShippingTime(),
+                                vendorB.getArticleOffer(article.getId()).getShippingTime()
+                            };
+                            if (shippingTimes[0] < shippingTimes[1]) return -1;
+                            if (shippingTimes[0] > shippingTimes[1]) return 1;
 
-                            if (shippingTimeA < shippingTimeB) return -1;
-                            if (shippingTimeB > shippingTimeA) return 1;
 
-                            // if shipping is the same, continue
-                            
-                            // sort by rating
-                            double ratings[] = {vendorA.getRating(), vendorB.getRating()};
-                            
+                            // if shipping is the same, continue with comparing by rating
+                            // higher == better
+                            double ratings[] = {vendorA.getRating(), vendorB.getRating()};                            
                             if (Double.compare(ratings[0], ratings[1]) > 0) return -1;
                             if (Double.compare(ratings[0], ratings[1]) < 0) return 1;
 
-                            // if ratring is the same, continue with number of ratings
 
-                            if (vendorA.getNumberOfRatings() > vendorB.getNumberOfRatings()) return -1;
-                            if (vendorA.getNumberOfRatings() < vendorB.getNumberOfRatings()) return 1;
+                            // if rating is the same, continue with comparing number of ratings
+                            // higher == better
+                            int numberOfRatings[] = {vendorA.getNumberOfRatings(), vendorB.getNumberOfRatings()};
+                            if (numberOfRatings[0] > numberOfRatings[1]) return -1;
+                            if (numberOfRatings[0] < numberOfRatings[1]) return 1;
                             
 
-                            // if number of rating is same, continue with availability
-                            
-                            int availability[] = {vendorA.getArticleOffer(article.getId()).getAvailability(), vendorB.getArticleOffer(article.getId()).getAvailability()};
-
-                            // higher availability == better
+                            // if number of rating is same, continue with comparing availability qty  
+                            // higher == better
+                            int availability[] = {
+                                vendorA.getArticleOffer(article.getId()).getAvailability(), 
+                                vendorB.getArticleOffer(article.getId()).getAvailability()
+                            };
                             if (availability[0] > availability[1]) return -1;
                             if (availability[0] < availability[1]) return 1;
-                            
-
-                        // sort by rating, then by availability 
-                        /*
-                            if (shippingTimeA == shippingTimeB) {
-                                vendorA.getRating()
-                            }
-                        */
 
                             return 0;
-
                         })
                         .collect(Collectors.toList());
 
-
-
-
-
-        return f.get(0);
+        return vendorsSortedByCriteria.get(0);
     }
 
     public Vendor findFairBestVendor(Article article) {
 
-        List<Vendor> vendorList = Vendor.getVendorsForArticle(article.getId());
 
-        double lowestPrice = getLowestPriceOfVendors(vendorList, article);
+        List<Vendor> vendorsWithLowestPrice = getVendorsWithLowestPrice(article);
 
-        List<Vendor> filteredByPrice = vendorsByPrice(vendorList, article, lowestPrice);
-        
-
-        Double lowestSelectionCount = filteredByPrice.stream()
-            .map(vendor -> selectionCount.getOrDefault(vendor.getId(), (double) 0))
+        Double minVendorSelectionCount = vendorsWithLowestPrice.stream()
+            .map(vendor -> vendorSelectionCount.getOrDefault(vendor.getId(), 0.0))
             .reduce(Double.POSITIVE_INFINITY, Double::min);
 
-        List<Vendor> filteredBySelectionCount = filteredByPrice.stream()
-            .filter(vendor -> {
-                return (Double.compare(selectionCount.getOrDefault(vendor.getId(), (double) 0), lowestSelectionCount) == 0);
-            })
+        List<Vendor> vendorsWithLowestSelectionCount = vendorsWithLowestPrice.stream()
+            .filter(vendor -> (Double.compare(vendorSelectionCount.getOrDefault(vendor.getId(), 0.0), minVendorSelectionCount) == 0))
             .collect(Collectors.toList());
 
-        Random rand = new Random();
-        Vendor bestVendor = filteredBySelectionCount.get(rand.nextInt(filteredBySelectionCount.size()));
+        Vendor bestVendor;
 
+        if (vendorsWithLowestSelectionCount.size() == 1) {
+            bestVendor = vendorsWithLowestSelectionCount.get(0);
+        } else {
+            Random rand = new Random();
+            bestVendor = vendorsWithLowestSelectionCount.get(rand.nextInt(vendorsWithLowestSelectionCount.size()));
+        }
 
-        selectionCount.put(bestVendor.getId(), selectionCount.getOrDefault(bestVendor.getId(), (double) 0) + 1);
+        vendorSelectionCount.put(bestVendor.getId(), vendorSelectionCount.getOrDefault(bestVendor.getId(), 0.0) + 1);
 
         return bestVendor;
 
     }
 
-    private double getLowestPriceOfVendors(List<Vendor> vendorList, Article article) {
+    private double getLowestPriceOfVendors(List<Vendor> vendors, Article article) {
         
-        return vendorList
-                .stream()
+        return vendors.stream()
                 .map(vendor -> vendor.getArticleOffer(article.getId()).getPrice())
                 .reduce(Double.POSITIVE_INFINITY, Double::min);
     }
 
-    private List<Vendor> vendorsByPrice(List<Vendor> vendorList, Article article, double lowestPrice) {
-        
-        return vendorList.stream()
-                .filter(vendor -> {
-                    double currentVendorPrice = vendor.getArticleOffer(article.getId()).getPrice();
-                    return (Double.compare(currentVendorPrice, lowestPrice) == 0);
-                })
-                .collect(Collectors.toList());
+    private List<Vendor> getVendorsWithLowestPrice(Article article) {
+
+        List<Vendor> articleVendors = Vendor.getVendorsForArticle(article.getId());
+
+        double lowestPrice = getLowestPriceOfVendors(articleVendors, article);
+
+        List<Vendor> vendorsWithLowestPrice = articleVendors.stream()
+            .filter(vendor -> {
+                double currentVendorPrice = vendor.getArticleOffer(article.getId()).getPrice();
+                return (Double.compare(currentVendorPrice, lowestPrice) == 0);
+            })
+            .collect(Collectors.toList());
+
+        return vendorsWithLowestPrice;
     }
 
 }
