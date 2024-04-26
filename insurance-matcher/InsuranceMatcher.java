d04a2a5e5219b9b8a6b00fb11d0c9cb844de198a
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-04-26 20:38:59 +0200

    (insurance-matcher): add initial state by EH

---
+++ insurance-matcher/InsuranceMatcher.java
@@ -0,0 +1,14 @@
+package de.entwicklerheld.insuranceMatching;
+
+class InsuranceMatcher {
+
+    static InsuranceMatcherResult getBestMatch(
+            Insurance[] insuranceDatabase,
+            InsuranceType insuranceType,
+            InsurancePreference usersInsurancePreference
+    ) {
+        return null;
+    }
+
+
+}
