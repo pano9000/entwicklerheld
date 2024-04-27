bd26e953799bbc0ec5294d33666ce999220b3f2e
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-04-27 14:52:34 +0200

    (emoji-bot): add my solution

--- emoji-bot/SlackBot.java
+++ emoji-bot/SlackBot.java
@@ -1,18 +1,47 @@
 package io.polylith.emotionaltracking;
 
 import com.vdurmont.emoji.EmojiParser;
-import com.vdurmont.emoji.EmojiLoader;
+import com.vdurmont.emoji.EmojiManager;
+import com.vdurmont.emoji.Emoji;
+
 import me.ramswaroop.jbot.core.common.JBot;
 import me.ramswaroop.jbot.core.slack.Bot;
 import redis.clients.jedis.Jedis;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import redis.clients.jedis.JedisPool;
+import java.util.List;
+import java.util.ArrayList;
 
 @JBot
 public class SlackBot extends Bot {
 
-    public void processEmojiMessage(String userId, String text, EmojiLists emojiLists) throws NotImplementedException {
-        // Here is your coding playground - CODE HERE!
-        throw new NotImplementedException();
+    public void processEmojiMessage(String userId, String text, EmojiLists emojiLists) {
+
+        if (userId != "UNKNOWN") {
+            JedisPool pool = new JedisPool(this.getRedisHost(), this.getRedisPort());
+
+            try (Jedis jedis = pool.getResource()) {
+
+                List<Emoji> blackListedEmojis = new ArrayList<Emoji>();
+                for (String emoji : emojiLists.getBlacklistEmojis()) {
+                    blackListedEmojis.add(EmojiManager.getForAlias(emoji));
+                }
+
+                String cleanedText = EmojiParser.removeEmojis(
+                    EmojiParser.parseToUnicode(text), 
+                    blackListedEmojis
+                );
+
+                List<String> extractedEmojis = EmojiParser.extractEmojis(cleanedText);
+                if (extractedEmojis.size() > 0) {
+                    jedis.set(userId, extractedEmojis.get(extractedEmojis.size() - 1));
+                }
+
+            }
+
+            pool.close();
+        }
+
+
     }
 
     public String getRedisHost() {
