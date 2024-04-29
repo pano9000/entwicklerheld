46e7debc13c3ea0b735f7f334969e1897e3a31cc
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-04-29 19:31:23 +0200

    (weigh-the-slow-loris): add my solution

--- weigh-the-slow-loris/Scale.java
+++ weigh-the-slow-loris/Scale.java
@@ -1,16 +1,35 @@
 package de.entwicklerheld.scalechallenge;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.List;
-
 
 class Scale {
     static List<Integer> getMasses(Integer weight, List<Integer> allMasses) {
-        // You need to implement this method.
-        // You can also add attributes to the class and add new methods or functions
-        throw new NotImplementedException();
+        return getBestPath(weight, allMasses, new HashMap<Integer, List<Integer>>());
+    }
+
+    static private List<Integer> getBestPath(Integer target, List<Integer> masses, HashMap<Integer, List<Integer>> memo) {
+      if (memo.get(target) != null) return memo.get(target);
+      if (target == 0) return new ArrayList<>();
+      if (target < 0) return null;
+
+      List<Integer> shortestPath = null;
+
+      for (int mass : masses) {
+        int remainder = target - mass;
+        List<Integer> remainderPath = getBestPath(remainder, masses, memo);
+        if (remainderPath != null) {
+            List<Integer> currentPath = new ArrayList<>(remainderPath);
+            currentPath.add(mass);
+            if (shortestPath == null || currentPath.size() < shortestPath.size()) {
+                shortestPath = currentPath;
+            }
+        }
+      }
+
+      memo.put(target, shortestPath);
+      return shortestPath;
     }
 }
