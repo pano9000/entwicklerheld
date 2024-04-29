e2fa3354c809e499bc8f339530641c6c9ddf274f
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-04-29 19:29:15 +0200

    (weigh-the-slow-loris): add initial state by EH

---
+++ weigh-the-slow-loris/Scale.java
@@ -0,0 +1,16 @@
+package de.entwicklerheld.scalechallenge;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+class Scale {
+    static List<Integer> getMasses(Integer weight, List<Integer> allMasses) {
+        // You need to implement this method.
+        // You can also add attributes to the class and add new methods or functions
+        throw new NotImplementedException();
+    }
+}
