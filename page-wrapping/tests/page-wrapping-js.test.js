328b7bf7c48d9e51a427506cf6e2228615ed515a
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-04-30 11:51:48 +0200

    (page-wrapping): modify tests for node testrunner/chai

--- page-wrapping/tests/page-wrapping-js.test.js
+++ page-wrapping/tests/page-wrapping-js.test.js
@@ -1,9 +1,10 @@
-import { buildPages } from "../page-wrapping-js";
+import { buildPages } from "../page-wrapping-js.js";
 import { html1, html2, html3 } from "../text-data.js";
+import {expect} from "chai";
+import domino from "domino";
+import fs from "fs"
+import {beforeEach, afterEach, after, test} from "node:test"
 
-const domino = require('domino');
-
-let fs = require('fs');
 let json = [];
 let visData = [];
 let dom;
@@ -30,6 +31,7 @@ beforeEach(() => {
     document.body.appendChild(displayNode);
 
     console.log("##polylith[testStarted");
+
 });
 
 afterEach(() => {
@@ -40,7 +42,9 @@ afterEach(() => {
     }
 });
 
-afterAll(() => {
+
+
+after(() => {
     let result = JSON.stringify(json);
     fs.writeFileSync('visualization-data.json', result);
 });
@@ -51,13 +55,13 @@ test('test_1_1', () => {
     const actual= buildPages(data, displayNode, pageHeight);
 
     if (!actual || !actual.pages) {
-        expect(false, `Your function should return an object {'pages': pages, 'hasToc': ${hasToc}}`).toBe(true);
+        expect(false, `Your function should return an object {'pages': pages, 'hasToc': ${hasToc}}`).to.be.true;
     }
 
     visData = [1,1, html1];
 
-    expect(actual.pages.length, `Expected length of the pages array is 3, but was ${actual.pages.length}`).toBe(3);
-    expect(actual.hasToc, `Expected parameter hasToc should be ${hasToc}, but was ${actual.hasToc}`).toBe(hasToc);
+    expect(actual.pages.length, `Expected length of the pages array is 3, but was ${actual.pages.length}`).to.be.eql(3);
+    expect(actual.hasToc, `Expected parameter hasToc should be ${hasToc}, but was ${actual.hasToc}`).to.deep.equal(hasToc);
 });
 
 test('test_2_1', () => {
@@ -66,32 +70,33 @@ test('test_2_1', () => {
     const actual= buildPages(data, displayNode, pageHeight);
 
     if (!actual || !actual.pages) {
-        expect(false, `Your function should return an object {'pages': pages, 'hasToc': ${hasToc}}`).toBe(true);
+        expect(false, `Your function should return an object {'pages': pages, 'hasToc': ${hasToc}}`).to.be.true;
     }
 
     visData = [2, 1, html2];
 
-    expect(actual.pages.length, `Expected length of the pages array is 9, but was ${actual.pages.length}`).toBe(9);
-    expect(actual.hasToc, `Expected parameter hasToc should be ${hasToc}, but was ${actual.hasToc}`).toBe(hasToc);
+    expect(actual.pages.length, `Expected length of the pages array is 9, but was ${actual.pages.length}`).to.eql(9);
+    expect(actual.hasToc, `Expected parameter hasToc should be ${hasToc}, but was ${actual.hasToc}`).to.deep.equal(hasToc);
 
     displayNode.innerHTML = actual.pages[0];
     const tocNode = document.getElementById('paged-toc-node');
-    if (!tocNode) expect(false, `The first page should be the table of contents containing a node with id 'paged-toc-node', but was null.`).toBe(true);
+
+    if (!tocNode) expect(false, `The first page should be the table of contents containing a node with id 'paged-toc-node', but was null.`).to.be.true;
 
     let classToken = 'paged-toc-h1';
     let headings = document.getElementsByClassName(classToken);
     let expectedHeadingsCount = 1;
-    if (!headings || headings.length === 0) expect(false, `The first page should be the table of contents with all headings of the text having class '${classToken}', but was null.`).toBe(true);
-    expect(headings.length, `There should be ${expectedHeadingsCount} h1 heading with class '${classToken}' in the ToC, but was ${headings.length}`).toBe(expectedHeadingsCount);
+    if (!headings || headings.length === 0) expect(false, `The first page should be the table of contents with all headings of the text having class '${classToken}', but was null.`).to.be.true;
+    expect(headings.length, `There should be ${expectedHeadingsCount} h1 heading with class '${classToken}' in the ToC, but was ${headings.length}`).to.be.eql(expectedHeadingsCount);
     let headingText = headings[0].textContent;
-    expect(headingText.toLowerCase(), `There should be ${expectedHeadingsCount} h1 heading with class '${classToken}' and Text 'Table of Contents' in the ToC, but was '${headingText}'`).toBe('table of contents');
+    expect(headingText.toLowerCase(), `There should be ${expectedHeadingsCount} h1 heading with class '${classToken}' and Text 'Table of Contents' in the ToC, but was '${headingText}'`).to.be.eql('table of contents');
 
     classToken = 'paged-toc';
     headings = document.getElementsByClassName(classToken);
     expectedHeadingsCount = 10;
 
-    if (!headings || headings.length === 0) expect(true, `The first page should be the table of contents with all headings of the text having class '${classToken}', but was null.`).toBe(true);
-    expect(headings.length, `There should be ${expectedHeadingsCount} h2 headings with class '${classToken}' in the ToC, but was ${headings.length}`).toBe(expectedHeadingsCount);
+    if (!headings || headings.length === 0) expect(true, `The first page should be the table of contents with all headings of the text having class '${classToken}', but was null.`).to.be.true;
+    expect(headings.length, `There should be ${expectedHeadingsCount} h2 headings with class '${classToken}' in the ToC, but was ${headings.length}`).to.deep.equal(expectedHeadingsCount);
 });
 
 test('test_2_2', () => {
@@ -100,32 +105,32 @@ test('test_2_2', () => {
     const actual= buildPages(data, displayNode, pageHeight);
 
     if (!actual || !actual.pages) {
-        expect(false, `Your function should return an object {'pages': pages, 'hasToc': ${hasToc}}`).toBe(true);
+        expect(false, `Your function should return an object {'pages': pages, 'hasToc': ${hasToc}}`).to.be.true;
     }
 
     visData = [2, 2, html3];
 
-    expect(actual.pages.length, `Expected length of the pages array is 8, but was ${actual.pages.length}`).toBe(8);
-    expect(actual.hasToc, `Expected parameter hasToc should be ${hasToc}, but was ${actual.hasToc}`).toBe(hasToc);
+    expect(actual.pages.length, `Expected length of the pages array is 8, but was ${actual.pages.length}`).to.be.equal(8);
+    expect(actual.hasToc, `Expected parameter hasToc should be ${hasToc}, but was ${actual.hasToc}`).to.deep.equal(hasToc);
 
     displayNode.innerHTML = actual.pages[0];
     const tocNode = document.getElementById('paged-toc-node');
-    if (!tocNode) expect(false, `The first page should be the table of contents containing a node with id 'paged-toc-node', but was null.`).toBe(true);
+    if (!tocNode) expect(false, `The first page should be the table of contents containing a node with id 'paged-toc-node', but was null.`).to.be.true;
 
     let classToken = 'paged-toc-h1';
     let headings = document.getElementsByClassName(classToken);
     let expectedHeadingsCount = 1;
-    if (!headings || headings.length === 0) expect(false, `The first page should be the table of contents with all headings of the text having class '${classToken}', but was null.`).toBe(true);
-    expect(headings.length, `There should be ${expectedHeadingsCount} h1 heading with class '${classToken}' in the ToC, but was ${headings.length}`).toBe(expectedHeadingsCount);
+    if (!headings || headings.length === 0) expect(false, `The first page should be the table of contents with all headings of the text having class '${classToken}', but was null.`).to.be.true;
+    expect(headings.length, `There should be ${expectedHeadingsCount} h1 heading with class '${classToken}' in the ToC, but was ${headings.length}`).to.be.eql(expectedHeadingsCount);
     let headingText = headings[0].textContent;
-    expect(headingText.toLowerCase(), `There should be ${expectedHeadingsCount} h1 heading with class '${classToken}' and Text 'Table of Contents' in the ToC, but was '${headingText}'`).toBe('table of contents');
+    expect(headingText.toLowerCase(), `There should be ${expectedHeadingsCount} h1 heading with class '${classToken}' and Text 'Table of Contents' in the ToC, but was '${headingText}'`).to.be.eql('table of contents');
 
     classToken = 'paged-toc';
     headings = document.getElementsByClassName(classToken);
     expectedHeadingsCount = 7;
 
-    if (!headings || headings.length === 0) expect(false, `The first page should be the table of contents with all headings of the text having class '${classToken}', but was null.`).toBe(true);
-    expect(headings.length, `There should be ${expectedHeadingsCount} h2 headings with class '${classToken}' in the ToC, but was ${headings.length}`).toBe(expectedHeadingsCount);
+    if (!headings || headings.length === 0) expect(false, `The first page should be the table of contents with all headings of the text having class '${classToken}', but was null.`).to.be.true;
+    expect(headings.length, `There should be ${expectedHeadingsCount} h2 headings with class '${classToken}' in the ToC, but was ${headings.length}`).to.be.eql(expectedHeadingsCount);
 });
 
 function mockData(data) {
@@ -152,17 +157,17 @@ function writeVisualisation() {
 }
 
 function setVisualization(scenarioId, sentenceId, textData) {
-    let code = fs.readFileSync("page-wrapping-js/page-wrapping-js.js", 'utf8');
+    let code = fs.readFileSync("./page-wrapping-js.js", 'utf8');
     code = code.replace(/export /g, '');
 
-    let iframe_content = fs.readFileSync("page-wrapping-js/visualization/iframe-template.html", 'utf8');
+    let iframe_content = fs.readFileSync("./visualization/iframe-template.html", 'utf8');
     iframe_content = iframe_content.replace(/##CODE##/g, String(code));
 
     iframe_content = iframe_content.replace(/'/g, '\\\'');
     iframe_content = iframe_content.replace(/"/g, '&quot;');
     iframe_content = iframe_content.replace(/(?:\r\n|\r|\n)/g, '\\n');
 
-    let visualizationContent = fs.readFileSync("page-wrapping-js/visualization/visualization-template.html", 'utf8');
+    let visualizationContent = fs.readFileSync("./visualization/visualization-template.html", 'utf8');
     visualizationContent = visualizationContent.replace(/##IFRAMECONTENT##/g,iframe_content);
     json.push({
         "scenarioId": String(scenarioId),
