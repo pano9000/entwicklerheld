097d3d9b19f87714745a04e2ea49263afb8fe797
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-05-17 23:03:24 +0200

    (luhn-checksum): add my solution

--- luhn-checksum/LuhnAlgorithm.cs
+++ luhn-checksum/LuhnAlgorithm.cs
@@ -6,8 +6,28 @@ namespace LuhnCsharpImplementation
     {
         public static Boolean IsValid(String number)
         {
-            // implement me
-            return false;
+            char[] cleanedNumberChar = number.Replace(" ", "").ToCharArray();
+            if (cleanedNumberChar.Length < 2) return false;
+
+            int sum = 0;
+            bool odd = true;
+            for (int i = cleanedNumberChar.Length - 1; i >= 0; i--) {
+                if (!System.Char.IsDigit(cleanedNumberChar[i])) return false;
+                int currentInt = cleanedNumberChar[i] - '0';
+                odd = !odd;
+
+                if (!odd) {
+                    sum += currentInt;
+                    continue;
+                }
+
+                int doubledDigit = currentInt * 2;
+                doubledDigit = doubledDigit > 9 ? (doubledDigit - 9) : doubledDigit;
+                sum += doubledDigit;
+             }
+
+            return (sum % 10 == 0);
+
         }
     }
 }
