362b366e938c1d9e9ae07cd02303cb66f2d230b5
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-05-17 09:07:55 +0200

    (armstrong-numbers-python): add my solution

--- armstrong-numbers-python/task.py
+++ armstrong-numbers-python/task.py
@@ -1,3 +1,13 @@
 def is_armstrong_number(number):
-    # TODO: implement this function
-    return False
+    
+    if (number > -1 and number < 10):
+        return True
+    if (number >= 10 and number <= 99):
+        return False
+
+    sum = 0
+    strNumber = str(number)
+    for digit in strNumber:
+        sum += int(digit)**len(strNumber)
+    
+    return sum == number
