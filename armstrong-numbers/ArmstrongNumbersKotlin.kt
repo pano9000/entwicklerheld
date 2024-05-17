c3bd28f572416dc2b2f899f417349cf28e1b56d9
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-05-17 08:37:26 +0200

    (armstrong-numbers): add my solution

--- armstrong-numbers/ArmstrongNumbersKotlin.kt
+++ armstrong-numbers/ArmstrongNumbersKotlin.kt
@@ -1,6 +1,22 @@
 package de.entwicklerheld.armstrongNumbersKotlin
 
 fun isArmstrongNumber(numberToCheck: Int): Boolean {
-    // TODO: implement this function
-    return false
+
+    if (numberToCheck > -1 && numberToCheck < 10) return true;
+    if (numberToCheck >= 10 && numberToCheck <= 99) return false;
+
+    var sum = 0;
+    val numberStr = numberToCheck.toString();
+    for (charDigit in numberStr) {
+        var intDigit = charDigit.digitToInt();
+
+        //use for loop to avoid needing to convert to Double then Int again, if we used pow()
+        var currentSum = 1;
+        for (i in 1..numberStr.length) {
+            currentSum *= intDigit;
+        }
+        sum += currentSum
+    }
+
+    return sum == numberToCheck
 }
