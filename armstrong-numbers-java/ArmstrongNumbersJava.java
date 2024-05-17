0d2862d382b593b0d47f12db09c0f7049d02f2eb
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-05-17 08:59:30 +0200

    (armstrong-numbers-java): add my solution

--- armstrong-numbers-java/ArmstrongNumbersJava.java
+++ armstrong-numbers-java/ArmstrongNumbersJava.java
@@ -1,7 +1,27 @@
 package de.entwicklerheld.armstrongNumbersJava;
+
+
 public class ArmstrongNumbersJava {
     public static boolean isArmstrongNumber(int numberToCheck) {
-        // TODO: implement this function
-        return false;
+        if (numberToCheck > -1 && numberToCheck < 10) return true;
+        if (numberToCheck >= 10 && numberToCheck <= 99) return false;
+
+        int sum = 0;
+        char[] numberStr = Integer.toString(numberToCheck).toCharArray();
+
+        for (char charDigit : numberStr) {
+            int intDigit = Character.getNumericValue(charDigit);
+
+            //use for loop to avoid needing to convert to Double then Int again, if we used pow()
+            int currentSum = 1;
+            for (int i = 1; i<= numberStr.length; i++) {
+                currentSum *= intDigit;
+            }
+
+            sum += currentSum;
+        }
+
+        return (sum == numberToCheck);
+
     }
 }
