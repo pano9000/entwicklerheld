8c3152bdcc9842355b5a033c3d67b4a5d88ac3b4
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-05-17 09:18:01 +0200

    (armstrong-numbers-csharp): add my solution

--- armstrong-numbers-csharp/ArmstrongNumbersCsharp.cs
+++ armstrong-numbers-csharp/ArmstrongNumbersCsharp.cs
@@ -3,8 +3,16 @@ namespace ArmstrongNumbersCsharpImplementation
     public static class ArmstrongNumbersCsharp
     {
         public static bool IsArmstrongNumber(int numberToCheck) {
-            // TODO: implement this function
-            return false;
+            if (numberToCheck > -1 && numberToCheck < 10) return true;
+            if (numberToCheck >= 10 && numberToCheck <= 99) return false;
+
+            string numberStr = numberToCheck.ToString();
+            int sum = 0;
+            foreach (char charDigit in numberStr) {
+                double digit = System.Char.GetNumericValue(charDigit);
+                sum += (int)(System.Math.Pow(digit, numberStr.Length));
+            }
+            return sum == numberToCheck;
         }
     }
 }
