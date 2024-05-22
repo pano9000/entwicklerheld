f60d6f871bbc82cc2b3fb397cf9246ba8ef44bad
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-05-22 09:36:15 +0200

    (new-spaceship-validation): add solution for stage 1

--- new-spaceship-validation/AccessController.java
+++ new-spaceship-validation/AccessController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 
 @RestController
@@ -23,12 +22,32 @@ public class AccessController {
     }
 
     public static boolean isValidShipId(String shipId) {
-        throw new NotImplementedException();
+        
+        int checksumAccumulator = 0;
+        for (int i = 0; i <= shipId.length() - 2; i++) {
+            checksumAccumulator += Character.getNumericValue(shipId.charAt(i)) * (i+1);
+        }
+        int checksumMod = checksumAccumulator % 11;
+        char finalCheckDigit = (checksumMod == 10) ? 'X' : Character.forDigit(checksumMod, 10);
+
+        return (shipId.charAt(shipId.length() - 1) == finalCheckDigit);
     }
 
     @RequestMapping(value = "/check-in", method = RequestMethod.POST)
     public ResponseEntity<MessageResponse> checkIn(@RequestBody CheckInRequestObject request) {
-        throw new NotImplementedException();
+
+        MessageResponse responseMsg = new MessageResponse();
+
+        if (!AccessController.isValidShipId(request.shipID)) {
+            responseMsg.message = "Ship ID invalid. Defense system activated!";
+            return new ResponseEntity<>(responseMsg, HttpStatus.FORBIDDEN);
+        } 
+        
+        // should we check here, if the ship is already checked in, before checking in again?
+        this.checkInLog.checkIn(request.shipID);
+        responseMsg.message = "Ship ID valid. Welcome back!";
+        return new ResponseEntity<>(responseMsg, HttpStatus.OK);
+    
     }
 }
 
