83d312b320f54feb6a43e8975fea67d8c0a06cf7
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-05-22 08:30:40 +0200

    (christmas-tree-of-hanoi): add solution

--- christmas-tree-of-hanoi/christmas-tree-of-hanoi-javascript.js
+++ christmas-tree-of-hanoi/christmas-tree-of-hanoi-javascript.js
@@ -1,3 +1,5 @@
+import Move from "./Move.js";
+
 /**
  * @param {number} treeLayers
  * @param {string} sourceTreeStand
@@ -5,6 +7,16 @@
  * @param {string} auxiliaryTreeStand
  * @returns {Move[]}
  */
-export function christmasTreeOfHanoi(treeLayers, sourceTreeStand, destinationTreeStand, auxiliaryTreeStand) {
-  return [];
+ export function christmasTreeOfHanoi(treeLayers, sourceTreeStand, destinationTreeStand, auxiliaryTreeStand) {
+
+  function moveLayers(n, src, dst, aux, moves) {
+    if (n === 0) return;
+    moveLayers(n - 1, src, aux, dst, moves);
+    moves.push(new Move(n, src, dst));
+    moveLayers(n - 1, aux, dst, src, moves);
+  }
+
+  const moves = [];
+  moveLayers(treeLayers, sourceTreeStand, destinationTreeStand, auxiliaryTreeStand, moves);
+  return moves;
 }
