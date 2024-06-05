7723c7bd41ddab2f818c626640bf162cdb7e0f20
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-06-05 09:02:07 +0200

    (handle-the-shopping-cart): clean up / refactor final solution

--- handle-the-shopping-cart/CartService.java
+++ handle-the-shopping-cart/CartService.java
@@ -7,14 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpSession;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.List;
 import java.util.Collections;
 import java.util.Collection;
@@ -24,8 +23,7 @@ import java.util.Collection;
 @RestController
 public class CartService {
 
-    HashMap<String, Object> data = new HashMap<>();
-    //HashMap<String, Recommendation> recommendations = new HashMap<>();
+    HashMap<String, Object> userCarts = new HashMap<>();
     RecommendationService recommendationService = new RecommendationService();
 
     @Autowired
@@ -34,9 +32,18 @@ public class CartService {
     @RequestMapping(value = "/update-cart", method = RequestMethod.POST)
     @ResponseBody
     public ResponseEntity<String> updateCart() {
-        Object userId = httpSession.getAttribute("UserId");
-        //System.out.println(userId.toString());
-        data.put(userId.toString(), httpSession.getAttribute("Cart"));
+
+        String userId = (String) httpSession.getAttribute("UserId");
+        if (userId == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);    
+        }
+
+        List<String> sessionCart = getCartFromSession();
+
+        userCarts.put(
+            userId, 
+            (sessionCart != null) ? sessionCart : new LinkedList<String>()
+        );
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -44,77 +51,67 @@ public class CartService {
     @RequestMapping(value = "/get-cart", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<String>> getCart() {
         
-        Object userId = httpSession.getAttribute("UserId");
-        List<String> sessionCart = getSessionCart(userId.toString());
-        return new ResponseEntity<>(sessionCart, HttpStatus.OK);
+        String userId = (String) httpSession.getAttribute("UserId");
+        if (userId == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);    
+        }
+
+        List<String> userCart = getCartByUserId(userId);
+        httpSession.setAttribute("Cart", userCart);
+
+        return new ResponseEntity<>(userCart, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/checkout-cart", method = RequestMethod.POST)
     public ResponseEntity<String> checkoutCart() {
+        // ideally this should be dependency injected as function parameter
         CheckoutService checkoutService = CheckoutService.getInstance();
-        String userId = (String)httpSession.getAttribute("UserId");
-        List<String> sessionCart = getSessionCart(userId);
 
-        if (!checkoutService.checkoutCart(userId, sessionCart)) {
-            return new ResponseEntity<>(HttpStatus.I_AM_A_TEAPOT);    //TODO use correct http status
+        String userId = (String) httpSession.getAttribute("UserId");
+        if (userId == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);    
+        }
+
+        List<String> userCart = getCartByUserId(userId);
+
+        // throw error, if checkout fails
+        if (!checkoutService.checkoutCart(userId, userCart)) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         };
         
-        updateRecommendations(sessionCart);
-        //System.out.println(this.recommendationService.data.getOrDefault("0000000000012", new Recommendation("null")).buyCount);
-        data.remove(userId);            
+        recommendationService.updateRecommendationsFromCart(userCart);
+        userCarts.remove(userId);
+
         return new ResponseEntity<>(HttpStatus.OK);
-
     }
-
 
 
     @RequestMapping(value = "/get-recommendations", method = RequestMethod.GET)
     public ResponseEntity<List<String>> getRecommendations() {
-        System.out.println("-----------------GetRec-----: ");
-        //System.out.println(recommendationService.data.get("0000000000002").buyCount.toString());
 
         String userId = (String)httpSession.getAttribute("UserId");
-
-        List<String> cart = (List<String>)httpSession.getAttribute("Cart");
-        System.out.println("\t-----------cart----: " + cart.toString());
-
-        //List<String> sessionCart = getSessionCart(userId);
-        List<String> recommendedEans = new ArrayList<String>();
-        //System.out.println("GetRec-----------\n" + sessionCart.toString() + "\n---------------");
-        for (String item : cart) {
-            String res = recommendationService.getRecommendationForEAN(item, cart, recommendedEans);
-            System.out.println("\t\treccomedation for item: '" + item + "' // " + res);
-            recommendedEans.add(res);
+        if (userId == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);    
         }
 
-        //Collections.sort(recommendedEans);
+        // it is weird, that here the cart is only available from the session and not the stored user data
+        List<String> userCart = getCartFromSession();
+        List<String> recommendedEans = recommendationService.getRecommendationsForCart(userCart);
 
         return new ResponseEntity<>(recommendedEans, HttpStatus.OK);
-
     }
 
-    private List<String> getSessionCart(String userId) {
-        List<String> userCartObj = (List<String>)data.getOrDefault(userId, new LinkedList<String>());
-
-        return userCartObj;
-
+    private List<String> getCartByUserId(String userId) {
+        return (List<String>) userCarts.getOrDefault(userId, new LinkedList<String>());
     }
 
-    private void updateRecommendations(List<String> userCartItems) {
-    for (String item : userCartItems) {
-        recommendationService.addEntry(item, new Recommendation(item));
-        Recommendation currentRec = recommendationService.data.get(item);
-
-        for (String sItem : userCartItems) {
-            if (!sItem.equals(item)) {
-                currentRec.addBoughtWithEntry(sItem);
-            }
-        }
-
-        }
-
+    private List<String> getCartFromSession() {
+        Object httpSessionCart = httpSession.getAttribute("Cart");
+        return (httpSessionCart instanceof List) ? (List<String>) httpSessionCart : new LinkedList<String>();
     }
 
+
+    // this should've been a separate file ideally, but we cannot do that here :-(
     private class Recommendation {
         public String ean;
         public Integer buyCount;
@@ -122,123 +119,101 @@ public class CartService {
 
         public Recommendation(String ean) {
             this.ean = ean;
-            this.buyCount = 0;
+            this.buyCount = 1;
             this.boughtWith = new HashMap<String, Recommendation>();
         }
 
         public void addBoughtWithEntry(String boughtWithEan) {
-            boughtWith.putIfAbsent(boughtWithEan, new Recommendation(boughtWithEan));
-            Recommendation sCurrentRec = boughtWith.get(boughtWithEan);
-            sCurrentRec.buyCount++;
+            if (boughtWith.get(boughtWithEan) == null) {
+                boughtWith.put(boughtWithEan, new Recommendation(boughtWithEan));
+            } else {
+                boughtWith.get(boughtWithEan).buyCount++;
+            }
         }
 
+        @Override
+        public String toString() {
+            return String.format("EAN: %s | Buy Count: %d | boughtWith: %s", ean, buyCount, boughtWith.toString());
+        }
 
     }
 
+    // this should've been a separate file ideally, but we cannot do that here :-(
     private class RecommendationService {
-        public HashMap<String, Recommendation> data = new HashMap<String, Recommendation>();
-        public boolean addEntry(String addEan, Recommendation addRecommendation) {
-            Recommendation currEntry = data.get(addEan);
-            if (currEntry == null) {
-                data.put(addEan, addRecommendation);
-                return true;
-            }
-            currEntry.buyCount++;
+        public HashMap<String, Recommendation> recommendationData = new HashMap<String, Recommendation>();
 
-            for (Map.Entry<String, Recommendation> addRecEntry : addRecommendation.boughtWith.entrySet()) {
-                Recommendation w = addRecEntry.getValue();
-                currEntry.addBoughtWithEntry(w.ean);
+        public void addEntry(String ean, Recommendation recommendation) {
+
+            Recommendation currentEntry = recommendationData.get(ean);
+
+            if (currentEntry == null) {
+                recommendationData.put(ean, recommendation);
+            } else {
+                currentEntry.buyCount++;
             }
 
-            return true;
+            for (Recommendation addRecEntry : recommendation.boughtWith.values()) {
+                if (!ean.equals(addRecEntry.ean)) {
+                    currentEntry.addBoughtWithEntry(addRecEntry.ean);
+                }
+            }
+
         }
 
-        public String getRecommendationForEAN(String ean, List<String> cart, List<String> currentRecommedations) {
-            //List<String> highestBuyCountItems = getEanHighestBuyCountItems(ean);
-            List<Recommendation> highestBuyCountItems = getItemsSortedBySold(data.get(ean).boughtWith.values());
-            System.out.println("\t\t\tgetRecommendationForEAN: " + ean + " // " + highestBuyCountItems.toString());
-/*
-            // if ean is not in cart and is only one -> use that item
-            if (highestBuyCountItems.size() == 1 && !cart.contains(highestBuyCountItems.get(0))) {
-                return highestBuyCountItems.get(0);
-            }
-*/
+        public void updateRecommendationsFromCart(List<String> cart) {
+            for (String ean : cart) {
+                addEntry(ean, new Recommendation(ean));
+                Recommendation currentRec = recommendationService.recommendationData.get(ean);
 
-            // if ean is not in cart and is more then one -> get the one with higher individual buy count
-            for (Recommendation highCountItemEan : highestBuyCountItems) {
-                if (cart.contains(highCountItemEan.ean)) {
-                    System.out.println("\t\t\t\tisAlready in cart, skipping: " + highCountItemEan.ean + " // " + cart.contains(highCountItemEan.ean));
-                    continue;
+                for (String sEan : cart) {
+                    if (!sEan.equals(ean)) {
+                        currentRec.addBoughtWithEntry(sEan);
+                    }
+                }
+            }
+
+        }
+
+        public String getRecommendationForEAN(String ean, List<String> cart, List<String> recommendedEans) {
+            List<Recommendation> boughtWithEans = getItemsSortedBySold(recommendationData.get(ean).boughtWith.values());
+
+            // try to get suitable item from "boughtWith" first
+            for (Recommendation item : boughtWithEans) {
+                if (!cart.contains(item.ean)) {
+                    return item.ean;
                 } 
-                return highCountItemEan.ean;
             }
 
-            List<Recommendation> v = getItemsSortedBySold(data.values());
+            // try to get suitable item from all items, if boughtWith items are all already in the cart
+            List<Recommendation> allItems = getItemsSortedBySold(recommendationData.values());
+            for (Recommendation item : allItems) {
+                if (!cart.contains(item.ean) && !recommendedEans.contains(item.ean)) {
+                    return item.ean;
+                }
+            }
+
+            return null;
+        }
+
+
+        public List<String> getRecommendationsForCart(List<String> cart) {
             
-            for (Recommendation it : v) {
-                if (!cart.contains(it.ean) && !currentRecommedations.contains(it.ean)) {
-                    System.out.println("\t\t\t\tGetting most sold item not in cart: " + it.ean);
-                    return it.ean;
-                }
-            }
-            // if eans are all in cart -> get most sold item overall -> TODO method
-         /*   String mostSold = getMostSoldItemOverall();
-            if (!cart.contains(mostSold) && !currentRecommedations.contains(mostSold)) {
-                System.out.println("\t\t\tReturning MostSold: " + mostSold + " // ");
-                return mostSold;
-            }*/
-            return "";
+            List<String> recommendedEans = new ArrayList<String>();
 
-        }
-
-        private List<String> getEanHighestBuyCountItems(String ean) {
-            Recommendation currRec = data.get(ean);
-            List<String> abc = new ArrayList<>();
-
-            int maxBuyCount = 0;
-            for ( Recommendation recommendation : currRec.boughtWith.values()) {
-                System.out.println("\t\t\t" + recommendation.ean + " // " + recommendation.buyCount);
-
-                if (maxBuyCount < recommendation.buyCount) {
-                    maxBuyCount = recommendation.buyCount;
-                }
-
-            }
-
-            for (Recommendation recommendation : currRec.boughtWith.values()) {
-                if (recommendation.buyCount == maxBuyCount) {
-                    abc.add(recommendation.ean);
-                }
-            }
-            System.out.println("\t\t\thighest buycount: " +  maxBuyCount + ": " + abc.toString());
-            return abc;
-
-        }
-
-        private String getMostSoldItemOverall() {
-
-            int maxBuyCount = 0;
-            String ean = "";
-            for ( Recommendation recommendation : data.values()) {
-                if (maxBuyCount < recommendation.buyCount) {
-                    ean = recommendation.ean;
-                    maxBuyCount = recommendation.buyCount;
+            for (String item : cart) {
+                String recommendedEan = getRecommendationForEAN(item, cart, recommendedEans);
+                if (recommendedEan != null) {
+                    recommendedEans.add(recommendedEan);
                 }
             }
 
-            return ean;
+            Collections.sort(recommendedEans);
+            return recommendedEans;
         }
 
         private List<Recommendation> getItemsSortedBySold(Collection<Recommendation> originalItems) {
-
             List<Recommendation> items = new ArrayList<Recommendation>(originalItems);
-            //System.out.println("b: " + items.toString());
-
-            Collections.sort(items, (a, b) -> {
-                return b.buyCount - a.buyCount;
-             });
-            //System.out.println("after: " + items.toString());
-
+            Collections.sort(items, (a, b) -> b.buyCount - a.buyCount);
             return items;
         }
 
@@ -249,63 +224,4 @@ public class CartService {
         SpringApplication.run(CartService.class, args);
     }
 
-    private void unused() {
-                CheckoutService checkoutService = CheckoutService.getInstance();
-        System.out.println("checkoutdata size " + checkoutService.data.size());
-        System.out.println("checkiztjeyset " + checkoutService.data.keySet().toString());
-        /*
-            "0000000000005": {
-                buy_count: integer,
-                bought_with: {
-                    ean: Integer
-                }
-            }
-
-
-        */
-        HashMap<String, HashMap<String, Object>> abc = new HashMap<String, HashMap<String, Object>>();
-        for (List<String> eans : checkoutService.data.values()) {
-            System.out.println(eans.toString());
-            for (String ean : eans) {
-                System.out.println("\t" + ean);
-
-                abc.putIfAbsent(ean, new HashMap<String, Object>());
-
-                HashMap<String, Object> current = abc.get(ean);
-
-                current.putIfAbsent("buyCount", 0);
-                current.putIfAbsent("boughtWith", new HashMap<String, Integer>());
-
-                int buyCount = (int) current.get("buyCount");
-                current.put("buyCount", ++buyCount);
-
-            }
-        }
-
-
-        
-        for (HashMap.Entry<String, HashMap<String, Object>> entry : abc.entrySet()) {
-            System.out.println(entry.getKey());
-            String hashEan = entry.getKey();
-
-            for (List<String> eans : checkoutService.data.values()) {
-                if (!eans.contains(hashEan)) {
-                    continue;
-                }
-                HashMap<String, Integer> boughtWith = (HashMap)entry.getValue().get("boughtWith");
-
-                for (String checkoutEan : eans) {
-                    if (!checkoutEan.equals(hashEan)) {
-                        Integer buyCountBoughtWith = boughtWith.getOrDefault(checkoutEan, 0);
-                        boughtWith.put(checkoutEan, ++buyCountBoughtWith);
-                    }
-                }
-
-            }
-
-            System.out.println("\t" + entry.getValue().toString());
-
-        }
-    }
 }
-
