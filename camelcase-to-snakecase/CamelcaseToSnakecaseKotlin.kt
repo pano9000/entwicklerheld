84873b8789e5ef3e5016d7f0d6571886e1d65be2
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-06-06 08:20:20 +0200

    (camelcase-to-snakecase): add final solution cleaned up and bufixed
    (previous version did not correctly recursively also rename list entries)

--- camelcase-to-snakecase/CamelcaseToSnakecaseKotlin.kt
+++ camelcase-to-snakecase/CamelcaseToSnakecaseKotlin.kt
@@ -1,75 +1,39 @@
 fun camelcaseToSnakeCase(map: Map<String, Any>): Map<String, Any> {
-    println("-----------Start: " + map.toString() + "------\n");
-    val convertedMap = mutableMapOf<String, Any>();
 
+    val convertedMap = mutableMapOf<String, Any>();
     for (key in map.keys) {
         val snakeKey = camelcaseToSnakeCaseKey(key);
-        //println(key + "//" + snakeKey)
+        val mapValue = map.get(key);
 
-        if (map.get(key) is Map<*, *>) {
-            @Suppress("UNCHECKED_CAST")
-            val submap = map.get(key) as Map<String, Any>;
-            
-            convertedMap.put(snakeKey, camelcaseToSnakeCase(submap));
-            println(key + " // recursive added to new map as " + snakeKey)
-        } 
-        else if (map.get(key) is List<*>) {
-            println("list found // " + snakeKey);
-            var sublist = map.get(key) as List<*>;
-
-            //camelcaseToSnakeCase(sublist)
-            var b =  mutableListOf<Any>();
-
-            for (item in sublist) {
-                println("in list: " + item.toString())
-
-                if (item is Map<*, *>) {
-                    println("item is map")
-                    //val submap = map.get(key) as Map<String, Any>;
-                    b.add(camelcaseToSnakeCase(item as Map<String, Any>));
-                    println("result " + b)
-
-                    
-                    //var result = camelcaseToSnakeCase(item as Map<String, Any>);
-                    //println("result " + result)
-                }
-
-            }
-            convertedMap.put(snakeKey, b);
-
+        when (mapValue) {
+            is Map<*, *> -> convertedMap.put(snakeKey, camelcaseToSnakeCase(mapValue as Map<String, Any>));
+            is List<*> -> convertedMap.put(snakeKey, camelcaseToSnakeCase(mapValue));
+            else -> convertedMap.put(snakeKey, mapValue as Any)
         }
-        else {
-            val valuee = map.get(key) as Any;
-            println(key + " // added to new map as " + snakeKey)
-            convertedMap.put(snakeKey, valuee)
-        }
-        
     }
-
-    println("---------------End: " + convertedMap.toString() + "\n----------\n");
 
     return convertedMap.toMap()
 }
 
-/*
 fun camelcaseToSnakeCase(list: List<*>): List<*> {
-    println("Start Listi: " + list.toString())
-    val convertedList = mutableListOf<Any>();
 
-    for (entry in list) {
-        convertedList.add(camelcaseToSnakeCase(entry))
-        
+    val convertedList =  mutableListOf<Any>();
+    for (item in list) {
+        when (item) {
+            is Map<*, *> -> convertedList.add(camelcaseToSnakeCase(item as Map<String, Any>));
+            is List<*> -> convertedList.add(camelcaseToSnakeCase(item))
+            else -> convertedList.add(item as Any);
+        }
     }
-    return convertedList
+    return convertedList;
 }
- */
 
 fun camelcaseToSnakeCaseKey(camelKey: String): String {
     var snakeKey = "";
     for (i in 0..camelKey.length - 1) {
         snakeKey += camelKey[i].lowercase();
 
-        if (i+1 > camelKey.length - 1) {
+        if (i + 1 > camelKey.length - 1) {
             continue;
         }
         
@@ -80,7 +44,6 @@ fun camelcaseToSnakeCaseKey(camelKey: String): String {
 
     return snakeKey
 }
-
 
 
 /**
