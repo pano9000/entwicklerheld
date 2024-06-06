acbe6700a3b1b5691937bc1d7ecd97722180e22c
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-06-06 07:33:08 +0200

    (doubly-linked-list): add my solution

--- doubly-linked-list/LinkedListJava.java
+++ doubly-linked-list/LinkedListJava.java
@@ -2,23 +2,53 @@ package de.entwicklerheld.linkedListJava;
 
 
 final class LinkedListJava<T> {
-    private Element<T> head;
+    private Element<T> head = new Element<T>(null, null, null);
 
     void push(T value) {
-        // scenario 1
+
+        if (head.getValue() == null) {
+            head = new Element<T>(value, null, null);
+            head.setPrev(head);
+            head.setNext(head);
+        } else {
+            Element<T> currentTail = head.getPrev();
+            Element<T> newTail = new Element<T>(value, currentTail, head);
+            currentTail.setNext(newTail);
+            head.setPrev(newTail);
+        }
+
     }
 
     T pop() {
-        // scenario 1
-        return null;
+
+        if (head.getValue() == null) {
+            return null;
+        }
+
+        Element<T> tail = head.getPrev();
+        T value = tail.getValue();
+        head.setPrev(tail.getPrev());
+        
+        return value;
+
     }
 
     T shift() {
-        // scenario 2
-        return null;
+
+        if (head.getValue() == null) {
+            return null;
+        }
+
+        T value = head.getValue();
+        head = head.getNext();
+        return value;
+
     }
 
     void unshift(T value) {
-        // scenario 3
+
+        push(value);
+        head = head.getPrev();
+
     }
 }
