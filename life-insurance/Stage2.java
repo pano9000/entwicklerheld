1c4218ea751ff6ad7fd83fc929302a3de07496b7
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-06-08 12:52:52 +0200

    (life-insurance): add solution for Stage2

--- life-insurance/Stage2.java
+++ life-insurance/Stage2.java
@@ -13,12 +13,20 @@ import entwicklerheldchallenge.Risk;
 
 
 public class Stage2 {
-	private static final BigDecimal HANDLING_COSTS_IN_PERCENT = new BigDecimal(2);
+	private static final BigDecimal HANDLING_COSTS_IN_PERCENT = new BigDecimal("0.02");
 	private static final BigDecimal NO_RISK = new BigDecimal(0);
-	private static final BigDecimal SMOKING_SURCHARGE_IN_PERCENT = new BigDecimal(3);
-	private static final BigDecimal RISKY_PROFESSIONAL_GROUP_SURCHARGE_IN_PERCENT = new BigDecimal(5);
-	private static final BigDecimal EXTREME_ATHLETE_SURCHARGE_IN_PERCENT = new BigDecimal(8);
-
+	private static final BigDecimal SMOKING_SURCHARGE_IN_PERCENT = new BigDecimal("0.03");
+	private static final BigDecimal RISKY_PROFESSIONAL_GROUP_SURCHARGE_IN_PERCENT = new BigDecimal("0.05");
+	private static final BigDecimal EXTREME_ATHLETE_SURCHARGE_IN_PERCENT = new BigDecimal("0.08");
+	private static final BigDecimal TWELVE_MONTHS = new BigDecimal(12);
+	private static final Map<Risk, BigDecimal> riskToPercentage = new HashMap<Risk, BigDecimal>();
+	
+	{
+		riskToPercentage.put(Risk.NOTHING, NO_RISK);
+		riskToPercentage.put(Risk.SMOKER, SMOKING_SURCHARGE_IN_PERCENT);
+		riskToPercentage.put(Risk.PROFESSIONAL_GROUP, RISKY_PROFESSIONAL_GROUP_SURCHARGE_IN_PERCENT);
+		riskToPercentage.put(Risk.EXTREME_ATHLETE, EXTREME_ATHLETE_SURCHARGE_IN_PERCENT);
+	}
 
 	@RequestMapping(path = "/payout", method = RequestMethod.GET)
 	public int getPayout(
@@ -26,8 +34,21 @@ public class Stage2 {
 			@RequestParam int lifetimeInYears,
 			@RequestParam List<Risk> risks
 	) {
-		// Implement this method
-		// You can also add new methods or functions
-		throw new RuntimeException("Not implemented yet.");
+		
+		BigDecimal monthlyDeductionRate = HANDLING_COSTS_IN_PERCENT;
+		for (Risk risk : risks) {
+			monthlyDeductionRate = monthlyDeductionRate.add(riskToPercentage.get(risk));	
+		}
+		
+		BigDecimal monthlyPaymentBD = BigDecimal.valueOf(monthlyPayment);
+        BigDecimal monthlyPayout = monthlyPaymentBD.subtract(monthlyPaymentBD.multiply(monthlyDeductionRate));
+        
+        BigDecimal lifetimePayout = 
+            monthlyPayout
+            .multiply(TWELVE_MONTHS)
+            .multiply(BigDecimal.valueOf(lifetimeInYears))
+        ;
+
+		return lifetimePayout.intValue();
 	}
 }
