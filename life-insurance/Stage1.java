c04b4bc8503d6e6f489425021480ed3c2a3bb637
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-06-08 12:21:38 +0200

    (life-insurance): add my solution for Stage1

--- life-insurance/Stage1.java
+++ life-insurance/Stage1.java
@@ -11,8 +11,9 @@ import java.math.BigDecimal;
 @RestController
 @RequestMapping(path = "/api")
 public class Stage1 {
-    private static final BigDecimal HANDLING_COSTS_IN_PERCENT = new BigDecimal("2");
-    private static final BigDecimal SMOKING_SURCHARGE_IN_PERCENT = new BigDecimal("3");
+    private static final BigDecimal HANDLING_COSTS_IN_PERCENT = new BigDecimal("0.02");
+    private static final BigDecimal SMOKING_SURCHARGE_IN_PERCENT = new BigDecimal("0.03");
+    private static final BigDecimal TWELVE_MONTHS = new BigDecimal("12");
 
 
     @RequestMapping(path = "/payout", method = RequestMethod.GET)
@@ -21,8 +22,18 @@ public class Stage1 {
             @RequestParam int lifetimeInYears,
             @RequestParam boolean isSmoker
     ) {
-        // Implement this method
-        // You can also add new methods or functions
-        throw new RuntimeException("Not implemented yet.");
+
+        BigDecimal monthlyDeductionRate = (isSmoker) ? HANDLING_COSTS_IN_PERCENT.add(SMOKING_SURCHARGE_IN_PERCENT) : HANDLING_COSTS_IN_PERCENT;
+
+        BigDecimal monthlyPaymentBD = BigDecimal.valueOf(monthlyPayment);
+        BigDecimal monthlyPayout = monthlyPaymentBD.subtract(monthlyPaymentBD.multiply(monthlyDeductionRate));
+        
+        BigDecimal lifetimePayout = 
+            monthlyPayout
+            .multiply(TWELVE_MONTHS)
+            .multiply(BigDecimal.valueOf(lifetimeInYears))
+        ;
+
+        return lifetimePayout.intValue();
     }
 }
