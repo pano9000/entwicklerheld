399220f10a9ab29677b8f456199a36f1ff51a814
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-06-08 22:29:41 +0200

    (book-store): add initial state

---
+++ book-store/BookStoreJava.java
@@ -0,0 +1,10 @@
+package de.entwicklerheld.bookStoreJava;
+
+import java.util.List;
+
+class BookStore {
+    public double calculateBasketCost(final List<Integer> books) {
+        // TODO: implement this function
+        return 0;
+    }
+}
