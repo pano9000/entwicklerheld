0ec9ae7db30bd6cf7a6115245f56dbcc7d8c3092
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-06-09 22:46:45 +0200

    (make-the-world-a-better-place): add initial state for stage1

---
+++ make-the-world-a-better-place/Stage1.java
@@ -0,0 +1,24 @@
+package de.entwicklerheld.betterplace.challenge.stage1;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+class Stage1 {
+
+    // STAGE 1
+
+    public static class Assert {
+
+        public static void assertEquals(final Object expected, final Object actual, final String message) {
+            // STAGE 1
+        }
+
+        public static void fail(final String message) {
+            // STAGE 1
+        }
+
+    }
+
+}
