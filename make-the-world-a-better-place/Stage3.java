e6e8664d16c23952fa97655f841b1568e6e0deb7
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-06-12 09:10:48 +0200

    (make-the-world-a-better-place): add stage3 solution

--- make-the-world-a-better-place/Stage3.java
+++ make-the-world-a-better-place/Stage3.java
@@ -7,12 +7,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.net.URL;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 
 public class Stage3 {
 
@@ -47,7 +47,72 @@ public class Stage3 {
         }
 
         public static void runTestMethod(final TestResult testResult, final Method method) {
-            // STAGE 3
+
+            if (method.getAnnotation(Stage1.Test.class) != null) {
+                Stage2.TestRunner.runTestMethod(testResult, method);
+                return;
+            } 
+            
+            ParameterizedTest parameterizedTest = method.getAnnotation(ParameterizedTest.class);
+
+            if (parameterizedTest.ignore()) {
+                testResult.getIgnoredTests().add(method);
+                return;
+            }
+
+            try {
+                Class<?> methodClass = method.getDeclaringClass();
+
+                Method getParametersMethod = methodClass.getMethod(parameterizedTest.parameterProvider());
+                @SuppressWarnings("unchecked") 
+                List<TestParameters> testParams = (List<TestParameters>) getParametersMethod.invoke(methodClass.newInstance());
+
+                for (TestParameters testParameter : testParams) {
+                    testResult.getRunnedTests().add(method);
+
+                    try {
+                        // to enable private method to be invoked
+                        method.setAccessible(true);
+                        method.invoke(
+                            (
+                                Modifier.isStatic(method.getModifiers()) ?
+                                null :
+                                methodClass.newInstance()
+                            ), 
+                            testParameter.expectedResult, testParameter.parameters);
+                    }
+                    catch(InvocationTargetException e) {
+                        String message = String.format(
+                            "▶ %s (%s)" +
+                            "\n\t✖ %s" +
+                            "\n\t%s",
+                            method.getName(),
+                            methodClass.toString(),
+                            e.getTargetException().toString(),
+                            testParameter
+                        );
+
+                        if (e.getTargetException() instanceof AssertionError) {
+                            testResult.getFailures().add(message);   
+                        } else {
+                            testResult.getErrors().add(message);
+                        }
+                        continue;
+                    }
+                    testResult.getSuccesses().add(method.getName());
+
+                }
+
+            }
+            // catch errors caused by the test runner -> shouldn't be counted as testresult, but print a message instead
+            catch(Exception e) {
+                System.out.println(
+                    String.format(
+                        "Invoking method '%s' failed, this should not happen and is potentially a bug in the testrunner:\n\t%s", 
+                        method.getName(),
+                        e.toString())
+                    );
+            }        
         }
     }
 
