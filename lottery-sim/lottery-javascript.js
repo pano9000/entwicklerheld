5a614c55699b32956933bdef5d47619cb2288cbe
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-06-14 08:21:15 +0200

    (lottery-sim): add initial state be EH

---
+++ lottery-sim/lottery-javascript.js
@@ -0,0 +1,19 @@
+export function generateLotteryTicket() {
+  // scenario 1
+  return [];
+}
+
+export function checkIfWinner(ticket, winningNumbers) {
+  // scenario 2
+  return 0;
+}
+
+export function simulateLottery(draws) {
+  // scenario 3
+  return {
+      userTicket: [],
+      totalWinCount: 0,
+      totalWinnings: 0,
+      draws: []
+  };
+}
