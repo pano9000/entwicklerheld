65cc0d115d6c863b7e22f76fac4eb0b24aacf49b
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-07-05 23:43:03 +0200

    (analyse-the-plant): refactor stage2 solution

--- analyse-the-plant/AnalyseThePlant.java
+++ analyse-the-plant/AnalyseThePlant.java
@@ -8,61 +8,40 @@ import de.entwicklerheld.analyse_the_plant.plant.Job;
 import de.entwicklerheld.analyse_the_plant.plant.WorkCenter;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.*;
-import java.util.Arrays;
 import java.util.Collections;
 
 public class AnalyseThePlant {
     public static int compareTaskTime(final Job job) {
-
         return (job.getTaskList().get(1).getTime() <= job.getTaskList().get(0).getTime()) ? 2 : 1;
-
     }
 
     public static List<Job> getListOfOrderedJobs(List<Job> jobList) {
-        //scenario 2: here you have to implement the Johnson rules
         //https://www.geeksforgeeks.org/johnsons-rule-in-sequencing-problems/
         //https://www.slideshare.net/slideshow/9johnsons-ruleppt/252543135#5
 
-        //List<Job> orderedJobList = new ArrayList<Job>(jobList.size());
         List<Job> orderedJobList = new ArrayList<>(Collections.nCopies(jobList.size(), (Job) null));
 
+        //copy array as original is immutable
         List<Job> copyJobList = new ArrayList<Job>(jobList.size());
-        //copy array
         for (Job job : jobList) {
             copyJobList.add(job);
         }
 
-        System.out.println("-------------start");
-
-        int i = 0;
         while (copyJobList.size() >= 1) {
             int jobIndex = getJobWithShortestTime(copyJobList);
             Job shortestTimeJob = copyJobList.get(jobIndex);
 
-            int nextIndex = getNextIndex(orderedJobList, compareTaskTime(shortestTimeJob));
-            System.out.println("nextIndex: " + nextIndex);
+            // decide, if to put value to the next free value at the 'start' or the 'end' of the ordered list
+            int nextIndex = getNextIndexInOrderedList(orderedJobList, compareTaskTime(shortestTimeJob));
+
             orderedJobList.set(nextIndex, copyJobList.get(jobIndex));
             copyJobList.remove(jobIndex);
-            System.out.println(String.format(
-                "joblist Size: %d | OrderedJob size: %d | currI: %d | orderedJobKist: %s", 
-                copyJobList.size(),
-                orderedJobList.size(),
-                i,
-                orderedJobList.toString()
-            ));
-
-            i++;
-
         }
-       System.out.println("-------------end");
-
         return orderedJobList;
     }
 
-    private static int getNextIndex(List<Job> orderedJobList, int direction) {
+    private static int getNextIndexInOrderedList(List<Job> orderedJobList, int direction) {
 
         if (direction == 1) {
             for (int i = 0; i < orderedJobList.size(); i++) {
@@ -91,7 +70,7 @@ public class AnalyseThePlant {
         int shortestTime = jobList.get(0).getTaskList().get(0).getTime();
 
         for (int i = 0; i < jobList.size(); i++) {
-        //for (Job job : jobList) {
+
             int currA = jobList.get(i).getTaskList().get(0).getTime();
             int currB = jobList.get(i).getTaskList().get(1).getTime();
 
@@ -117,24 +96,24 @@ public class AnalyseThePlant {
 
         Schedule schedule = new Schedule();
 
-        int taskACount = 0;
-        int taskBCount = 0; 
-        for (int k = 0; k < jobList.size(); k++) {
-            
-            Task taskA = jobList.get(k).getTaskList().get(0);
-            Task taskB = jobList.get(k).getTaskList().get(1);
+        int timeCountA = 0;
+        int timeCountB = 0; 
+
+        for (Job job : jobList) {  
+            Task taskA = job.getTaskList().get(0);
+            Task taskB = job.getTaskList().get(1);
 
             schedule.add(WorkCenter.A, taskA);
-            taskACount += taskA.getTime();
+            timeCountA += taskA.getTime();
 
-            if (taskBCount < taskACount) {
-                int idleTime = taskACount - taskBCount;
+            if (timeCountB < timeCountA) {
+                int idleTime = timeCountA - timeCountB;
                 schedule.add(WorkCenter.B, new IdleTask(WorkCenter.B, idleTime));
-                taskBCount += idleTime;
+                timeCountB += idleTime;
             }
 
             schedule.add(WorkCenter.B, taskB);
-            taskBCount += taskB.getTime();
+            timeCountB += taskB.getTime();
 
         }
 
