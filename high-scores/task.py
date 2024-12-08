562a68ecb36747462a72acbde50806d1adebc1fb
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-12-08 19:26:15 +0100

    (high-scores): add my solution

--- high-scores/task.py
+++ high-scores/task.py
@@ -1,12 +1,13 @@
-class HighScores:
+class HighScores:
     def __init__(self, scores):
         self.scores = scores
 
     def latest(self):
-        raise NotImplementedError
+        return self.scores[-1]
 
     def personal_best(self):
-        raise NotImplementedError
+        return max(self.scores)
 
     def personal_top_three(self):
-        raise NotImplementedError
+        sorted_scores = sorted(self.scores, reverse=True)
+        return sorted_scores if len(sorted_scores) < 3 else sorted_scores[0:3]
