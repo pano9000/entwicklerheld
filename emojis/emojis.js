184ae4bee0539f880ea57c2fc57cd7a162bf4595
Panagiotis Papadopoulos <pano_90@gmx.net>
2025-08-03 00:44:59 +0200

    (emojis): add my solution

--- emojis/emojis.js
+++ emojis/emojis.js
@@ -1,5 +1,28 @@
-export function replaceEmojis(text) {
-  // TODO: implement this function
-  throw Error('Not implemented yet!')
-}
+const emojiShortcodeRegEx = /:[\w]+:/ig;
 
+const allowedEmojis = new Set([
+  "angry",
+  "happy",
+  "love",
+  "nerdy",
+  "sad",
+  "shocked",
+  "sleepy",
+  "thumb",
+  "tongue",
+]);
+
+/**
+ * @param {string} text 
+ */
+export function replaceEmojis(text) {
+
+  return text.replaceAll(emojiShortcodeRegEx, (match) => {
+    const normalizedMatch = match.toLowerCase().replaceAll(":", "");
+
+    return (allowedEmojis.has(normalizedMatch)) 
+      ? `<span class="emoji emoji-${normalizedMatch}"> </span>`
+      : match;
+  })
+
+}
